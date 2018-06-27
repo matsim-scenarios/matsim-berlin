77fdaf05fee3b608dd1896325d8738e05fa52f63
ikaddoura <kaddoura@vsp.tu-berlin.de>
2018-06-27 13:11:51 -0400

    testing the reduced config...

--- src/test/java/org/matsim/run/RunBerlinScenarioTest.java
+++ src/test/java/org/matsim/run/RunBerlinScenarioTest.java
@@ -62,7 +62,7 @@ public class RunBerlinScenarioTest {
 	
 	// 1pct, testing the scores in the first and second iteration
 	@Test
-	public final void test2() {
+	public final void test2a() {
 		try {
 			Config config;
 
@@ -84,15 +84,65 @@ public class RunBerlinScenarioTest {
 		}
 	}
 	
+	// 1pct, testing the scores in the first and second iteration
+	@Test
+	public final void test2b() {
+		try {
+			Config config;
+
+			String configFile = "scenarios/berlin-v5.0-1pct-2018-06-18/input/berlin-5.0_config_reduced.xml";
+			config = ConfigUtils.loadConfig(configFile);
+			config.controler().setLastIteration(1);
+			config.qsim().setEndTime(30 * 3600.); // TODO: adjust in the config file!
+			config.strategy().setFractionOfIterationsToDisableInnovation(1.);
+			config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+			config.controler().setOutputDirectory( utils.getOutputDirectory() );
+			
+			Controler controler = RunBerlinScenario.run(config);
+			
+			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 111.84928125172443, controler.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
+			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 109.27928497494945, controler.getScoreStats().getScoreHistory().get(ScoreItem.average).get(1), MatsimTestUtils.EPSILON);
+
+		} catch ( Exception ee ) {
+			Logger.getLogger(this.getClass()).fatal("there was an exception: \n" + ee ) ;			
+			Assert.fail("Wasn't able to run the berlin scenario.");
+		}
+	}
+	
 	// 0.1pct, testing the scores in the first and second iteration
 	@Test
-	public final void test3() {
+	public final void test3a() {
 		try {
 			Config config;
 
 			String configFile = "scenarios/berlin-v5.0-0.1pct-2018-06-18/input/berlin-5.0_config.xml";
 			config = ConfigUtils.loadConfig(configFile);
 			config.controler().setLastIteration(1);
+			config.strategy().setFractionOfIterationsToDisableInnovation(1.);
+			config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+			config.controler().setOutputDirectory( utils.getOutputDirectory() );
+			
+			Controler controler = RunBerlinScenario.run(config);
+			
+			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 84.66967102654722, controler.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
+			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 84.76497660204434, controler.getScoreStats().getScoreHistory().get(ScoreItem.average).get(1), MatsimTestUtils.EPSILON);
+
+		} catch ( Exception ee ) {
+			Logger.getLogger(this.getClass()).fatal("there was an exception: \n" + ee ) ;			
+			Assert.fail("Wasn't able to run the berlin scenario.");
+		}
+	}
+	
+	// 0.1pct, testing the scores in the first and second iteration
+	@Test
+	public final void test3b() {
+		try {
+			Config config;
+
+			String configFile = "scenarios/berlin-v5.0-0.1pct-2018-06-18/input/berlin-5.0_config_reduced.xml";
+			config = ConfigUtils.loadConfig(configFile);
+			config.controler().setLastIteration(1);
+			config.qsim().setEndTime(30 * 3600.); // TODO: adjust in the config file!
 			config.strategy().setFractionOfIterationsToDisableInnovation(1.);
 			config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 			config.controler().setOutputDirectory( utils.getOutputDirectory() );
