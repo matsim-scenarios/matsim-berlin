43bf45281b2646c9a86f827869d35a3a300a974e
ikaddoura <kaddoura@vsp.tu-berlin.de>
2018-11-23 15:07:22 +0100

    add emissions analysis

---
+++ src/main/java/org/matsim/analysis/RunOfflineAirPollutionAnalysis.java
@@ -0,0 +1,163 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2017 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.analysis;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.emissions.EmissionModule;
+import org.matsim.contrib.emissions.utils.EmissionsConfigGroup;
+import org.matsim.contrib.emissions.utils.EmissionsConfigGroup.HbefaRoadTypeSource;
+import org.matsim.contrib.emissions.utils.EmissionsConfigGroup.NonScenarioVehicles;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Injector;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.algorithms.EventWriterXML;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.vehicles.VehicleType;
+
+/**
+* @author ikaddoura
+*/
+
+public class RunOfflineAirPollutionAnalysis {
+	
+	final static String runDirectory = "public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/output-berlin-v5.2-1pct/";	
+	final static String runId = "berlin-v5.2-1pct";
+
+	final static String hbefaFileCold = "shared-svn/projects/detailedEval/matsim-input-files/hbefa-files/v3.2/EFA_ColdStart_vehcat_2005average.txt";
+	final static String hbefaFileWarm = "shared-svn/projects/detailedEval/matsim-input-files/hbefa-files/v3.2/EFA_HOT_vehcat_2005average.txt";
+	
+	public static void main(String[] args) {
+		
+		String rootDirectory = null;
+		
+		if (args.length == 1) {
+			rootDirectory = args[0];
+		} else {
+			throw new RuntimeException("Please set the root directory. Aborting...");
+		}
+		
+		if (!rootDirectory.endsWith("/")) rootDirectory = rootDirectory + "/";
+		
+		Config config = ConfigUtils.loadConfig(rootDirectory + runDirectory + runId + ".output_config.xml");
+		config.vehicles().setVehiclesFile(rootDirectory + runDirectory + runId + ".output_vehicles.xml.gz");
+		config.plans().setInputFile(null);
+		
+		EmissionsConfigGroup eConfig = ConfigUtils.addOrGetModule(config, EmissionsConfigGroup.class);
+		eConfig.setAverageColdEmissionFactorsFile(rootDirectory + hbefaFileCold);
+		eConfig.setAverageWarmEmissionFactorsFile(rootDirectory + hbefaFileWarm);
+		eConfig.setHbefaRoadTypeSource(HbefaRoadTypeSource.fromLinkAttributes);
+		eConfig.setNonScenarioVehicles(NonScenarioVehicles.ignore);
+		
+		final String emissionEventOutputFile = rootDirectory + runDirectory + runId + "." + config.controler().getLastIteration() + ".emission.events.offline.xml.gz";
+		final String eventsFile = rootDirectory + runDirectory + runId + ".output_events.xml.gz";
+		
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		
+		// network
+		for (Link link : scenario.getNetwork().getLinks().values()) {
+
+			double freespeed = Double.NaN;
+
+			if (link.getFreespeed() <= 13.888889) {
+				freespeed = link.getFreespeed() * 2;
+				// for non motorway roads, the free speed level was reduced
+			} else {
+				freespeed = link.getFreespeed();
+				// for motorways, the original speed levels seems ok.
+			}
+			
+			if(freespeed <= 8.333333333){ //30kmh
+				link.getAttributes().putAttribute("hbefa_road_type", "URB/Access/30");
+			} else if(freespeed <= 11.111111111){ //40kmh
+				link.getAttributes().putAttribute("hbefa_road_type", "URB/Access/40");
+			} else if(freespeed <= 13.888888889){ //50kmh
+				double lanes = link.getNumberOfLanes();
+				if(lanes <= 1.0){
+					link.getAttributes().putAttribute("hbefa_road_type", "URB/Local/50");
+				} else if(lanes <= 2.0){
+					link.getAttributes().putAttribute("hbefa_road_type", "URB/Distr/50");
+				} else if(lanes > 2.0){
+					link.getAttributes().putAttribute("hbefa_road_type", "URB/Trunk-City/50");
+				} else{
+					throw new RuntimeException("NoOfLanes not properly defined");
+				}
+			} else if(freespeed <= 16.666666667){ //60kmh
+				double lanes = link.getNumberOfLanes();
+				if(lanes <= 1.0){
+					link.getAttributes().putAttribute("hbefa_road_type", "URB/Local/60");
+				} else if(lanes <= 2.0){
+					link.getAttributes().putAttribute("hbefa_road_type", "URB/Trunk-City/60");
+				} else if(lanes > 2.0){
+					link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-City/60");
+				} else{
+					throw new RuntimeException("NoOfLanes not properly defined");
+				}
+			} else if(freespeed <= 19.444444444){ //70kmh
+				link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-City/70");
+			} else if(freespeed <= 22.222222222){ //80kmh
+				link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-Nat./80");
+			} else if(freespeed > 22.222222222){ //faster
+				link.getAttributes().putAttribute("hbefa_road_type", "RUR/MW/>130");
+			} else{
+				throw new RuntimeException("Link not considered...");
+			}			
+		}
+		
+		Id<VehicleType> carVehicleTypeId = Id.create("car", VehicleType.class);
+		Id<VehicleType> freightVehicleTypeId = Id.create("freight", VehicleType.class);
+		
+		// vehicles
+		scenario.getVehicles().getVehicleTypes().get(carVehicleTypeId).setDescription("BEGIN_EMISSIONSPASSENGER_CAR;average;average;averageEND_EMISSIONS");
+		scenario.getVehicles().getVehicleTypes().get(freightVehicleTypeId).setDescription("BEGIN_EMISSIONSHEAVY_GOODS_VEHICLE;average;average;averageEND_EMISSIONS");		
+		
+		// the following is copy paste from the example...
+		
+        EventsManager eventsManager = EventsUtils.createEventsManager();
+
+		AbstractModule module = new AbstractModule(){
+			@Override
+			public void install(){
+				bind( Scenario.class ).toInstance( scenario );
+				bind( EventsManager.class ).toInstance( eventsManager );
+				bind( EmissionModule.class ) ;
+			}
+		};;
+
+		com.google.inject.Injector injector = Injector.createInjector(config, module);
+
+        EmissionModule emissionModule = injector.getInstance(EmissionModule.class);
+
+        EventWriterXML emissionEventWriter = new EventWriterXML(emissionEventOutputFile);
+        emissionModule.getEmissionEventsManager().addHandler(emissionEventWriter);
+
+        MatsimEventsReader matsimEventsReader = new MatsimEventsReader(eventsManager);
+        matsimEventsReader.readFile(eventsFile);
+
+        emissionEventWriter.closeFile();
+	}
+
+}
+
