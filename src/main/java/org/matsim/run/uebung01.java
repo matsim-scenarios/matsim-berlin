2965adc884d8e16335219482d3bb947ee02016a6
Ricardo Ewert <ricardo.ewert@campus.tu-berlin.de>
2018-11-27 18:37:35 +0100

    Übung aktuallisiert inkl. xml Ausgabe

--- src/main/java/org/matsim/run/uebung01.java
+++ src/main/java/org/matsim/run/uebung01.java
@@ -9,12 +9,14 @@ import org.matsim.contrib.freight.carrier.CarrierCapabilities;
 import org.matsim.contrib.freight.carrier.CarrierCapabilities.FleetSize;
 import org.matsim.contrib.freight.carrier.CarrierImpl;
 import org.matsim.contrib.freight.carrier.CarrierPlan;
+import org.matsim.contrib.freight.carrier.CarrierPlanXmlWriterV2;
 import org.matsim.contrib.freight.carrier.CarrierService;
 import org.matsim.contrib.freight.carrier.CarrierVehicle;
 import org.matsim.contrib.freight.carrier.CarrierVehicleType;
 import org.matsim.contrib.freight.carrier.CarrierVehicleTypeLoader;
 import org.matsim.contrib.freight.carrier.CarrierVehicleTypes;
 import org.matsim.contrib.freight.carrier.Carriers;
+import org.matsim.contrib.freight.carrier.TimeWindow;
 import org.matsim.contrib.freight.jsprit.MatsimJspritFactory;
 import org.matsim.contrib.freight.jsprit.NetworkBasedTransportCosts;
 import org.matsim.contrib.freight.jsprit.NetworkRouter;
@@ -39,30 +41,45 @@ public class uebung01 {
 	public static void main(String[] args) {
 		
 //Create carrier with services and shipments
-
-		Carriers anbieter1 = new Carriers() ;
-		Carrier myCarrier = CarrierImpl.newInstance(Id.create("Fahrer1", Carrier.class));
+		Carriers anbieter = new Carriers() ;
+		Carrier myCarrier = CarrierImpl.newInstance(Id.create("BSR", Carrier.class));
+		
 //Service erstellen
-		CarrierService service1 = CarrierService.Builder.newInstance(Id.create("Service1", CarrierService.class),Id.createLinkId("i(1,4)"))
+		CarrierService service1 = CarrierService.Builder.newInstance(Id.create("Service1", CarrierService.class),Id.createLinkId(7))
 				.setName("Tonne1")
-				.setServiceDuration(0.2)
+				.setServiceDuration(300)
 				.setCapacityDemand(10)
+				.setServiceStartTimeWindow(TimeWindow.newInstance(21600,54000))
 				.build();
-		CarrierService service2 = CarrierService.Builder.newInstance(Id.create("Service2", CarrierService.class),Id.createLinkId("i(6,9)"))
+		CarrierService service2 = CarrierService.Builder.newInstance(Id.create("Service2", CarrierService.class),Id.createLinkId(12))
 				.setName("Tonne2")
-				.setServiceDuration(0.2)
+				.setServiceDuration(300)
 				.setCapacityDemand(15)
+				.setServiceStartTimeWindow(TimeWindow.newInstance(21600,54000))
+				.build();
+		CarrierService service3 = CarrierService.Builder.newInstance(Id.create("Service3", CarrierService.class),Id.createLinkId(6))
+				.setName("Tonne3")
+				.setServiceDuration(150)
+				.setCapacityDemand(8)
+				.setServiceStartTimeWindow(TimeWindow.newInstance(21600,54000))
+				.build();
+		CarrierService service4 = CarrierService.Builder.newInstance(Id.create("Service4", CarrierService.class),Id.createLinkId(8))
+				.setName("Tonne4")
+				.setServiceDuration(300)
+				.setCapacityDemand(12)
+				.setServiceStartTimeWindow(TimeWindow.newInstance(21600,54000))
 				.build();
 		myCarrier.getServices().add(service1);
 		myCarrier.getServices().add(service2);
+		myCarrier.getServices().add(service3);
+		myCarrier.getServices().add(service4);
 		//	myCarrier.getServices().add(createMatsimService("Service1", "i(3,9)", 2));
 		//	myCarrier.getServices().add(createMatsimService("Service2", "i(4,9)", 2));		
 		
 //FahrzeugTyp erstellen
+		CarrierVehicleType carrierVehType = CarrierVehicleType.Builder.newInstance(Id.create("MüllwagenTyp1", org.matsim.vehicles.VehicleType.class))
 
-		CarrierVehicleType carrierVehType = CarrierVehicleType.Builder.newInstance(Id.create("gridType", org.matsim.vehicles.VehicleType.class))
-
-				.setCapacity(20)
+				.setCapacity(50)
 				.setMaxVelocity(10)
 				.setCostPerDistanceUnit(0.0001)
 				.setCostPerTimeUnit(0.001)
@@ -72,22 +89,26 @@ public class uebung01 {
 // FahrzeugTyp zu FahrzeugTypen hinzufügen
 		CarrierVehicleTypes vehicleTypes = new CarrierVehicleTypes() ;
 		vehicleTypes.getVehicleTypes().put(carrierVehType.getId(), carrierVehType);
+		
 // Fahrzeug erstellen		
-		CarrierVehicle carrierVehicle = CarrierVehicle.Builder.newInstance(Id.create("gridVehicle", org.matsim.vehicles.Vehicle.class), Id.createLinkId("i(10,2)"))
-				.setEarliestStart(0.0).setLatestEnd(36000.0).setTypeId(carrierVehType.getId()).build();
+		CarrierVehicle carrierVehicle1 = CarrierVehicle.Builder.newInstance(Id.create("Müllwagen1", org.matsim.vehicles.Vehicle.class), Id.createLinkId(13))
+				.setEarliestStart(21600.0).setLatestEnd(28800.0).setTypeId(carrierVehType.getId()).build();
+		CarrierVehicle carrierVehicle2 = CarrierVehicle.Builder.newInstance(Id.create("Müllwagen2", org.matsim.vehicles.Vehicle.class), Id.createLinkId(13))
+				.setEarliestStart(28800.0).setLatestEnd(36000.0).setTypeId(carrierVehType.getId()).build();
+		
 // Dienstleister erstellen		
 		CarrierCapabilities.Builder ccBuilder = CarrierCapabilities.Builder.newInstance()
 				.addType(carrierVehType)
-				.addVehicle(carrierVehicle)
-				.setFleetSize(FleetSize.INFINITE);		
+				.addVehicle(carrierVehicle1).addVehicle(carrierVehicle2)
+				.setFleetSize(FleetSize.FINITE);				//Flottengröße; Anzahl der Fahrzeug, die CarrierVehicle erzeugt wurden
 		myCarrier.setCarrierCapabilities(ccBuilder.build());
 // Carriers hinzufügen
-		anbieter1.addCarrier(myCarrier);
+		anbieter.addCarrier(myCarrier);
+		
 // Fahrzeugtypen den Anbietern zuordenen
-		new CarrierVehicleTypeLoader(anbieter1).loadVehicleTypes(vehicleTypes);
+		new CarrierVehicleTypeLoader(anbieter).loadVehicleTypes(vehicleTypes);
 		
 // Netzwerk integrieren	und Kosten für jsprit 			
-
 		Network network = NetworkUtils.createNetwork();
 		new MatsimNetworkReader(network).readFile("scenarios/equil/networkBeispiel.xml"); 
 		Builder netBuilder = NetworkBasedTransportCosts.Builder.newInstance( network,vehicleTypes.getVehicleTypes().values());
@@ -109,6 +130,10 @@ public class uebung01 {
 		NetworkRouter.routePlan(carrierPlanServices,netBasedCosts);
 		myCarrier.setSelectedPlan(carrierPlanServices);
 		
+//Ausagbe in xml Datei
+		new CarrierPlanXmlWriterV2(anbieter).write("output/Uebung/output_Test01.xml");
+
+	
 
 				
 		
