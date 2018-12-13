09127a7ea822f369f83d4b067c7cef711371cfac
ikaddoura <kaddoura@vsp.tu-berlin.de>
2018-12-13 15:40:53 +0100

    let the test actually test something...

--- src/test/java/org/matsim/run/RunBerlinScenarioTest.java
+++ src/test/java/org/matsim/run/RunBerlinScenarioTest.java
@@ -26,11 +26,9 @@ import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.analysis.ScoreStatsControlerListener.ScoreItem;
-import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.Population;
@@ -60,8 +58,7 @@ public class RunBerlinScenarioTest {
 		Assert.assertTrue( true );
 	}
 	
-	// 1pct, testing the scores in iteration 0
-	// yy could also add it.1. kai, aug'18
+	// 1pct, testing the scores in iteration 0 and 1
 	@Test
 	public final void test1pctUntilIteration1() {
 		try {
@@ -70,25 +67,30 @@ public class RunBerlinScenarioTest {
 			
 			Config config =  berlin.prepareConfig() ;
 			config.controler().setLastIteration(1);
+			config.strategy().setFractionOfIterationsToDisableInnovation(1);
 			config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 			config.controler().setOutputDirectory( utils.getOutputDirectory() );
 			
-			//			Scenario scenario = berlin.prepareScenario() ;
-			//			downsample( scenario.getPopulation().getPersons(), 0.001 );
-			
 			berlin.run() ;
 			
-			//			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 115.26173800545439, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
-//			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 114.73387586329103, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
-			// (above number was for 10pct)
+			// Compare with: https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/
+			
+			Assert.assertEquals("The scores in iteration 0 differ from https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/.", 114.65161732321907, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
+			Assert.assertEquals("The scores in iteration 0 differ from https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/.", 114.65161732321907, berlin.getScoreStats().getScoreHistory().get(ScoreItem.executed).get(0), MatsimTestUtils.EPSILON);
+
+			Assert.assertEquals("The scores in iteration 1 differ from https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/.", 113.75743205180812, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(1), 0.001);
+			Assert.assertEquals("The scores in iteration 1 differ from https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/.", 112.82205604436822, berlin.getScoreStats().getScoreHistory().get(ScoreItem.executed).get(1), 0.001);
+
+			// The differences in the scores compared to the run in the public-svn are probably related to the pt raptor router
+			// which seems to produce slightly different results (e.g. in case two routes are identical).
+			// Thus the large epsilon. ihab, dec'18
 			
 		} catch ( Exception ee ) {
 			throw new RuntimeException(ee) ;
 		}
 	}
 	
-	// 10pct, testing the scores in iteration 0
-	// yy could also add it.1. kai, aug'18
+	// 10pct, testing the scores in iteration 0 and 1
 	@Test
 	public final void test10pctUntilIteration1() {
 		try {
@@ -97,42 +99,40 @@ public class RunBerlinScenarioTest {
 			
 			Config config =  berlin.prepareConfig() ;
 			config.controler().setLastIteration(1);
+			config.strategy().setFractionOfIterationsToDisableInnovation(1);
 			config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 			config.controler().setOutputDirectory( utils.getOutputDirectory() );
 			
-			//			Scenario scenario = berlin.prepareScenario() ;
-			//			downsample( scenario.getPopulation().getPersons(), 0.001 );
-			
 			berlin.run() ;
 			
-			//			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 115.26173800545439, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
-			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 114.73387586329103, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
+			Assert.assertEquals("Wrong avg. AVG score in iteration 0.", 115.29161554748528, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
+			Assert.assertEquals("Wrong avg. AVG score in iteration 1.", 114.42163063762338, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(1), 0.001);
 			
+			// The differences in the scores compared to the run in the public-svn are probably related to the pt raptor router
+			// which seems to produce slightly different results (e.g. in case two routes are identical).
+			// Thus the large epsilon. ihab, dec'18
+
 		} catch ( Exception ee ) {
 			throw new RuntimeException(ee) ;
 		}
 	}
 	
-	// 1pct (version 5.1)
-	// testing the score in the 0th and 100th iteration
-	// testing the modal split in the 100th iteration
+	// 1pct, testing the score and modal split in iteration 0 and 40.
 	@Test
 	public final void test1pctManyIterations() {
 		// (right now cannot run even up to 50 iterations because logfile becomes to long.  I can tell maven to send the output
 		// to file, but then we don't see anything.  So we will have to play around with the number of iterations.  Thus the
 		// imprecise name of the test.   kai, aug'18)
+		
+		final int iteration = 40;
 		try {
 			String configFilename = "scenarios/berlin-v5.2-1pct/input/berlin-v5.2-1pct.config.xml";
 			RunBerlinScenario berlin = new RunBerlinScenario( configFilename, "overridingConfig.xml") ;
 			
 			Config config = berlin.prepareConfig() ;
-			config.controler().setLastIteration(40);
+			config.controler().setLastIteration(iteration);
 			config.qsim().setEndTime(30 * 3600.);
 
-//			config.qsim().setNumberOfThreads(1); // to have it fully deterministic
-			// yy should be fully deterministic also with larger number of threads as long as it is always the same number.  kai, jul'18
-//			config.qsim().setNumberOfThreads( 6 );
-//			config.global().setNumberOfThreads( 6 );
 			config.qsim().setNumberOfThreads( 1 );
 			config.global().setNumberOfThreads( 1 );
 			// small number of threads in hope to consume less memory.  kai, jul'18
@@ -155,21 +155,32 @@ public class RunBerlinScenarioTest {
 			berlin.run() ;
 			
 			Gbl.assertNotNull( berlin.getScoreStats() );
-			Gbl.assertNotNull( berlin.getScoreStats().getScoreHistory() );
-//			testScores(berlin.getScoreStats().getScoreHistory());
-//			testModalSplit(analyzeModeStats(berlin.getPopulation()));
-			// yyyyyy commented out until tub build server is ironed out. kai, aug'18
+			Gbl.assertNotNull( berlin.getScoreStats().getScoreHistory() );			
+			Gbl.assertNotNull( berlin.getScoreStats().getScoreHistory().get( ScoreItem.average) );
+			Gbl.assertNotNull( berlin.getScoreStats().getScoreHistory().get( ScoreItem.average).get(0) );
+			Assert.assertEquals("The scores in iteration 0 differ from https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/.", 114.65161732321907, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(0), MatsimTestUtils.EPSILON);
+			
+			Gbl.assertNotNull( berlin.getScoreStats().getScoreHistory().get( ScoreItem.average).get(iteration) );
+			Assert.assertEquals("Major change in the avg. AVG score in iteration " + iteration + " compared to https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.2-1pct/.", 107.01557515903745, berlin.getScoreStats().getScoreHistory().get(ScoreItem.average).get(iteration), 1.0);
+				
+			Map<String,Double> modeCnt = analyzeModeStats(berlin.getPopulation());
+			
+			double sum = 0 ;
+			for ( Double val : modeCnt.values() ) {
+				sum += val ;
+			}
+			
+			Assert.assertEquals("Major change in the car trip share.", 0.42014517165522886, modeCnt.get("car") / sum, 0.02);
+			Assert.assertEquals("Major change in the pt trip share.", 0.19189514917434855, modeCnt.get("pt") / sum, 0.02);
+			Assert.assertEquals("Major change in the bicycle trip share.", 0.147850718181878, modeCnt.get("bicycle") / sum, 0.02);
+			Assert.assertEquals("Major change in the walk trip share.", 0.14716203560532787, modeCnt.get("walk") / sum, 0.02);
+			Assert.assertEquals("Change in the freight trip share.", 0.001511153806506515, modeCnt.get("freight") / sum, MatsimTestUtils.EPSILON);
+			Assert.assertEquals("Change in the ride trip share.", 0.09143577157671025, modeCnt.get("ride") / sum, MatsimTestUtils.EPSILON);
+			
 			
 		} catch ( Exception ee ) {
 			throw new RuntimeException(ee) ;
 		}
-	}
-	
-	private static void downsample( final Map map, final double sample ) {
-		final Random rnd = MatsimRandom.getLocalInstance();
-		log.warn( "map size before=" + map.size() ) ;
-		map.values().removeIf( person -> rnd.nextDouble()>sample ) ;
-		log.warn( "map size after=" + map.size() ) ;
 	}
 	
 	private static Map<String, Double> analyzeModeStats(Population population) {
@@ -198,27 +209,10 @@ public class RunBerlinScenarioTest {
 		return modeCnt;	
 	}
 	
-	private static void testScores(Map<ScoreItem, Map<Integer, Double>> scoreHistory) {
-		Gbl.assertNotNull( scoreHistory );
-		Gbl.assertNotNull( scoreHistory.get( ScoreItem.average) );
-		Gbl.assertNotNull( scoreHistory.get( ScoreItem.average).get(0) );
-		Assert.assertEquals("Major change in the avg. AVG score in iteration 0.", 115.2173655596178, scoreHistory.get(ScoreItem.average).get(0), 1.0);
-		Gbl.assertNotNull( scoreHistory.get( ScoreItem.average).get(100) );
-		Assert.assertEquals("Major change in the avg. AVG score in iteration 100.", 115.39338160261939, scoreHistory.get(ScoreItem.average).get(100), 1.0);
-	}
-
-	private static void testModalSplit(Map<String, Double> modeCnt) {
-		
-		double sum = 0 ;
-		for ( Double val : modeCnt.values() ) {
-			sum += val ;
-		}
-		
-		Assert.assertEquals("Major change in the car trip share (iteration 100).", 0.41707279676702186, modeCnt.get("car") / sum, 0.02);
-		Assert.assertEquals("Major change in the pt trip share (iteration 100)", 0.1932777710849971, modeCnt.get("pt") / sum, 0.02);
-		Assert.assertEquals("Major change in the bicycle trip share (iteration 100)", 0.1403804663286204, modeCnt.get("bicycle") / sum, 0.02);
-		Assert.assertEquals("Major change in the walk trip share (iteration 100)", 0.15878500476404298, modeCnt.get("walk") / sum, 0.02);
-		Assert.assertEquals("Change in the freight trip share (iteration 100)", 0.0014730201842096616, modeCnt.get("freight") / sum, MatsimTestUtils.EPSILON);
-		Assert.assertEquals("Change in the ride trip share (iteration 100)", 0.089010940871108, modeCnt.get("ride") / sum, MatsimTestUtils.EPSILON);
+	private static void downsample( final Map map, final double sample ) {
+		final Random rnd = MatsimRandom.getLocalInstance();
+		log.warn( "map size before=" + map.size() ) ;
+		map.values().removeIf( person -> rnd.nextDouble()>sample ) ;
+		log.warn( "map size after=" + map.size() ) ;
 	}
 }
