ab2f1fe51ed645bc9b06191baf0ad27ef84b543b
Ricardo Ewert <ricardo.ewert@campus.tu-berlin.de>
2019-02-07 13:41:28 +0100

    Umbenennung Umlaute

--- src/main/java/RunAbfall/Run_Abfall.java
+++ src/main/java/RunAbfall/Run_Abfall.java
@@ -10,9 +10,7 @@ import org.matsim.contrib.freight.carrier.CarrierCapabilities;
 import org.matsim.contrib.freight.carrier.CarrierCapabilities.FleetSize;
 import org.matsim.contrib.freight.carrier.CarrierImpl;
 import org.matsim.contrib.freight.carrier.CarrierPlan;
-import org.matsim.contrib.freight.carrier.CarrierPlanWriter;
 import org.matsim.contrib.freight.carrier.CarrierPlanXmlWriterV2;
-import org.matsim.contrib.freight.carrier.CarrierService;
 import org.matsim.contrib.freight.carrier.CarrierShipment;
 import org.matsim.contrib.freight.carrier.CarrierVehicle;
 import org.matsim.contrib.freight.carrier.CarrierVehicleType;
@@ -36,80 +34,90 @@ import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.replanning.GenericStrategyManager;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.scoring.SumScoringFunction;
-import org.matsim.core.scoring.SumScoringFunction.LegScoring;
 import org.matsim.vehicles.EngineInformation.FuelType;
 import org.matsim.vehicles.EngineInformationImpl;
 
-import com.graphhopper.jsprit.analysis.toolbox.Plotter;
 import com.graphhopper.jsprit.core.algorithm.VehicleRoutingAlgorithm;
 import com.graphhopper.jsprit.core.algorithm.box.SchrimpfFactory;
-import com.graphhopper.jsprit.core.algorithm.recreate.ScoringFunction;
 import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
 import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
 import com.graphhopper.jsprit.core.util.Solutions;
 
 public class Run_Abfall {
 
-	/*	Übung mit dem Ziel die Erstellung von services, VehicleTypes, Carriers etc.
+	/*
+	 * Übung mit dem Ziel die Erstellung von services, VehicleTypes, Carriers etc.
 	 * zu üben
 	 */
 
 	private static final String SCENARIOS_EQUIL_NETWORK_BEISPIEL_XML = "scenarios/equil/networkBeispiel.xml";
-	enum scenarioAuswahl {chessboard, Wilmersdorf};
+
+	enum scenarioAuswahl {
+		chessboard, Wilmersdorf, Charlottenburg
+	};
 
 	public static void main(String[] args) {
-		
+
 		scenarioAuswahl scenarioWahl = scenarioAuswahl.chessboard;
-		
-		//MATSim config
+
+		// MATSim config
 		Config config = ConfigUtils.createConfig();
-		
-		// (the directory structure is needed for jsprit output, which is before the controler starts.  Maybe there is a better alternative ...)
+
+		// (the directory structure is needed for jsprit output, which is before the
+		// controler starts. Maybe there is a better alternative ...)
 		config.controler().setOutputDirectory("output/Uebung/MATSim1");
-		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
-		new OutputDirectoryHierarchy( config.controler().getOutputDirectory(), config.controler().getRunId(), config.controler().getOverwriteFileSetting() ) ;
+		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+		new OutputDirectoryHierarchy(config.controler().getOutputDirectory(), config.controler().getRunId(),
+				config.controler().getOverwriteFileSetting());
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.overwriteExistingFiles);
-		
+
 		config.network().setInputFile(SCENARIOS_EQUIL_NETWORK_BEISPIEL_XML);
 		config.controler().setLastIteration(0);
 		config.global().setRandomSeed(4177);
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.overwriteExistingFiles);
-		
-		Scenario scenario = ScenarioUtils.loadScenario(config);	
 
-		
-		switch (scenarioWahl)  {
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		switch (scenarioWahl) {
 		case chessboard:
-			//TODO Hier alles zusammenbauen.
+			// TODO Hier alles zusammenbauen.
 			break;
 		case Wilmersdorf:
-			//TODO
+			// TODO
 			break;
+		case Charlottenburg:
+			// TODO
 		default:
 			new RuntimeException("no scenario selected.");
 		}
-		//Create carrier with services and shipments
-		Carriers anbieter = new Carriers() ;
+		// Create carrier with services and shipments
+		Carriers anbieter = new Carriers();
 		Carrier myCarrier = createShipments();
 
-		//FahrzeugTyp erstellen
+		// FahrzeugTyp erstellen
 		CarrierVehicleType carrierVehType = createVehicleType();
 		// FahrzeugTyp zu FahrzeugTypen hinzufügen
-		CarrierVehicleTypes vehicleTypes = new CarrierVehicleTypes() ;
+		CarrierVehicleTypes vehicleTypes = new CarrierVehicleTypes();
 		vehicleTypes.getVehicleTypes().put(carrierVehType.getId(), carrierVehType);
 
-		// Fahrzeug erstellen		
-		CarrierVehicle carrierVehicle1 = CarrierVehicle.Builder.newInstance(Id.create("Müllwagen1", org.matsim.vehicles.Vehicle.class), Id.createLinkId(130))
+		// Fahrzeug erstellen
+		CarrierVehicle carrierVehicle1 = CarrierVehicle.Builder
+				.newInstance(Id.create("GargabeTruck1", org.matsim.vehicles.Vehicle.class), Id.createLinkId(130))
 				.setEarliestStart(21600.0).setLatestEnd(54000.0).setTypeId(carrierVehType.getId()).build();
-		CarrierVehicle carrierVehicle2 = CarrierVehicle.Builder.newInstance(Id.create("Müllwagen2", org.matsim.vehicles.Vehicle.class), Id.createLinkId(130))
+		CarrierVehicle carrierVehicle2 = CarrierVehicle.Builder
+				.newInstance(Id.create("GargabeTruck2", org.matsim.vehicles.Vehicle.class), Id.createLinkId(130))
 				.setEarliestStart(21600.0).setLatestEnd(54000.0).setTypeId(carrierVehType.getId()).build();
 
-		// Dienstleister erstellen		
-		CarrierCapabilities.Builder ccBuilder = CarrierCapabilities.Builder.newInstance()
-				.addType(carrierVehType)
-				.addVehicle(carrierVehicle1).addVehicle(carrierVehicle2)
-				.setFleetSize(FleetSize.FINITE);				//Flottengröße; Anzahl der Fahrzeug, die CarrierVehicle erzeugt wurden
+		// Dienstleister erstellen
+		CarrierCapabilities.Builder ccBuilder = CarrierCapabilities.Builder.newInstance().addType(carrierVehType)
+				.addVehicle(carrierVehicle1).addVehicle(carrierVehicle2).setFleetSize(FleetSize.FINITE); // Flottengröße;
+																											// Anzahl
+																											// der
+																											// Fahrzeug,
+																											// die
+																											// CarrierVehicle
+																											// erzeugt
+																											// wurden
 		myCarrier.setCarrierCapabilities(ccBuilder.build());
 		// Carriers hinzufügen
 		anbieter.addCarrier(myCarrier);
@@ -117,14 +125,16 @@ public class Run_Abfall {
 		// Fahrzeugtypen den Anbietern zuordenen
 		new CarrierVehicleTypeLoader(anbieter).loadVehicleTypes(vehicleTypes);
 
-		// Netzwerk integrieren	und Kosten für jsprit 			
+		// Netzwerk integrieren und Kosten für jsprit
 		Network network = NetworkUtils.createNetwork();
-		new MatsimNetworkReader(network).readFile(SCENARIOS_EQUIL_NETWORK_BEISPIEL_XML); 
-		Builder netBuilder = NetworkBasedTransportCosts.Builder.newInstance( network,vehicleTypes.getVehicleTypes().values());
+		new MatsimNetworkReader(network).readFile(SCENARIOS_EQUIL_NETWORK_BEISPIEL_XML);
+		Builder netBuilder = NetworkBasedTransportCosts.Builder.newInstance(network,
+				vehicleTypes.getVehicleTypes().values());
 		final NetworkBasedTransportCosts netBasedCosts = netBuilder.build();
-		netBuilder.setTimeSliceWidth(1800);	
+		netBuilder.setTimeSliceWidth(1800);
 
-		//Build jsprit, solve and route VRP for carrierService only -> need solution to convert Services to Shipments
+		// Build jsprit, solve and route VRP for carrierService only -> need solution to
+		// convert Services to Shipments
 		VehicleRoutingProblem.Builder vrpBuilder = MatsimJspritFactory.createRoutingProblemBuilder(myCarrier, network);
 		vrpBuilder.setRoutingCost(netBasedCosts);
 		VehicleRoutingProblem problem = vrpBuilder.build();
@@ -134,30 +144,31 @@ public class Run_Abfall {
 		Collection<VehicleRoutingProblemSolution> solutions = algorithm.searchSolutions();
 		VehicleRoutingProblemSolution bestSolution = Solutions.bestOf(solutions);
 
-		//Routing bestPlan to Network
+		// Routing bestPlan to Network
 		CarrierPlan carrierPlanServices = MatsimJspritFactory.createPlan(myCarrier, bestSolution);
-		NetworkRouter.routePlan(carrierPlanServices,netBasedCosts);
+		NetworkRouter.routePlan(carrierPlanServices, netBasedCosts);
 		myCarrier.setSelectedPlan(carrierPlanServices);
 
-		//Ausagbe in xml Datei
-		//new CarrierPlanXmlWriterV2(anbieter).write("output/Uebung/output_Test01.xml");
-		//als Diagramm plotten
-		//new Plotter(problem,bestSolution.getRoutes()).plot("output/Uebung/plott_Test01", "carrier1");
+		new CarrierPlanXmlWriterV2(anbieter)
+				.write(scenario.getConfig().controler().getOutputDirectory() + "/jsprit_CarrierPlans_Test01.xml");
 
-		new CarrierPlanXmlWriterV2(anbieter).write(scenario.getConfig().controler().getOutputDirectory() + "/jsprit_CarrierPlans_Test01.xml");
-
-		final org.matsim.core.controler.Controler controler = new org.matsim.core.controler.Controler(scenario); //Warum auch immer so umständlich
+		final org.matsim.core.controler.Controler controler = new org.matsim.core.controler.Controler(scenario); // Warum
+																													// auch
+																													// immer
+																													// so
+																													// umständlich
 
 		CarrierScoringFunctionFactory scoringFunctionFactory = createMyScoringFunction2(scenario);
-		CarrierPlanStrategyManagerFactory planStrategyManagerFactory = createMyStrategymanager(); 
+		CarrierPlanStrategyManagerFactory planStrategyManagerFactory = createMyStrategymanager();
 
-		CarrierModule listener = new CarrierModule(anbieter, planStrategyManagerFactory, scoringFunctionFactory) ;
+		CarrierModule listener = new CarrierModule(anbieter, planStrategyManagerFactory, scoringFunctionFactory);
 		listener.setPhysicallyEnforceTimeWindowBeginnings(true);
-		controler.addOverridingModule(listener) ;
+		controler.addOverridingModule(listener);
 
 		controler.run();
-		
-		new CarrierPlanXmlWriterV2(anbieter).write(scenario.getConfig().controler().getOutputDirectory() + "/output_CarrierPlans_Test01.xml");
+
+		new CarrierPlanXmlWriterV2(anbieter)
+				.write(scenario.getConfig().controler().getOutputDirectory() + "/output_CarrierPlans_Test01.xml");
 
 	}
 
@@ -165,15 +176,11 @@ public class Run_Abfall {
 	 * @return
 	 */
 	private static CarrierVehicleType createVehicleType() {
-		return CarrierVehicleType.Builder.newInstance(Id.create("MüllwagenTyp1", org.matsim.vehicles.VehicleType.class))
+		return CarrierVehicleType.Builder
+				.newInstance(Id.create("GargabeTruckTyp1", org.matsim.vehicles.VehicleType.class))
 
-				.setCapacity(100)
-				.setMaxVelocity(10)
-				.setCostPerDistanceUnit(0.0001)
-				.setCostPerTimeUnit(0.001)
-				.setFixCost(130)
-				.setEngineInformation(new EngineInformationImpl(FuelType.diesel, 0.015))
-				.build();
+				.setCapacity(100).setMaxVelocity(10).setCostPerDistanceUnit(0.0001).setCostPerTimeUnit(0.001)
+				.setFixCost(130).setEngineInformation(new EngineInformationImpl(FuelType.diesel, 0.015)).build();
 	}
 
 	/**
@@ -182,32 +189,24 @@ public class Run_Abfall {
 	private static Carrier createShipments() {
 		Carrier myCarrier = CarrierImpl.newInstance(Id.create("BSR", Carrier.class));
 
-		//Service erstellen
-		CarrierShipment shipment1 = CarrierShipment.Builder.newInstance(Id.create("Ship1",CarrierShipment.class),Id.createLinkId(7), Id.createLinkId(14), 10)
-				.setPickupServiceTime(300)
-				.setPickupTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryServiceTime(3600)
-				.build();
-		CarrierShipment shipment2 = CarrierShipment.Builder.newInstance(Id.create("Ship2",CarrierShipment.class),Id.createLinkId(12), Id.createLinkId(14), 15)
-				.setPickupServiceTime(300)
-				.setPickupTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryServiceTime(3600)
-				.build();
-		CarrierShipment shipment3 = CarrierShipment.Builder.newInstance(Id.create("Ship3",CarrierShipment.class),Id.createLinkId(6), Id.createLinkId(14), 8)
-				.setPickupServiceTime(300)
-				.setPickupTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryServiceTime(3600)
-				.build();
-		CarrierShipment shipment4 = CarrierShipment.Builder.newInstance(Id.create("Ship4",CarrierShipment.class),Id.createLinkId(8), Id.createLinkId(14), 12)
-				.setPickupServiceTime(300)
-				.setPickupTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryTimeWindow(TimeWindow.newInstance(21600,54000))
-				.setDeliveryServiceTime(3600)
-				.build();
-	
+		// Shipments erstellen
+		CarrierShipment shipment1 = CarrierShipment.Builder
+				.newInstance(Id.create("Ship1", CarrierShipment.class), Id.createLinkId(7), Id.createLinkId(14), 10)
+				.setPickupServiceTime(300).setPickupTimeWindow(TimeWindow.newInstance(21600, 54000))
+				.setDeliveryTimeWindow(TimeWindow.newInstance(21600, 54000)).setDeliveryServiceTime(3600).build();
+		CarrierShipment shipment2 = CarrierShipment.Builder
+				.newInstance(Id.create("Ship2", CarrierShipment.class), Id.createLinkId(12), Id.createLinkId(14), 15)
+				.setPickupServiceTime(300).setPickupTimeWindow(TimeWindow.newInstance(21600, 54000))
+				.setDeliveryTimeWindow(TimeWindow.newInstance(21600, 54000)).setDeliveryServiceTime(3600).build();
+		CarrierShipment shipment3 = CarrierShipment.Builder
+				.newInstance(Id.create("Ship3", CarrierShipment.class), Id.createLinkId(6), Id.createLinkId(14), 8)
+				.setPickupServiceTime(300).setPickupTimeWindow(TimeWindow.newInstance(21600, 54000))
+				.setDeliveryTimeWindow(TimeWindow.newInstance(21600, 54000)).setDeliveryServiceTime(3600).build();
+		CarrierShipment shipment4 = CarrierShipment.Builder
+				.newInstance(Id.create("Ship4", CarrierShipment.class), Id.createLinkId(8), Id.createLinkId(14), 12)
+				.setPickupServiceTime(300).setPickupTimeWindow(TimeWindow.newInstance(21600, 54000))
+				.setDeliveryTimeWindow(TimeWindow.newInstance(21600, 54000)).setDeliveryServiceTime(3600).build();
+
 		myCarrier.getShipments().add(shipment1);
 		myCarrier.getShipments().add(shipment2);
 		myCarrier.getShipments().add(shipment3);
@@ -215,7 +214,7 @@ public class Run_Abfall {
 		return myCarrier;
 	}
 
-	private static CarrierPlanStrategyManagerFactory createMyStrategymanager(){
+	private static CarrierPlanStrategyManagerFactory createMyStrategymanager() {
 		return new CarrierPlanStrategyManagerFactory() {
 			@Override
 			public GenericStrategyManager<CarrierPlan, Carrier> createStrategyManager() {
@@ -225,7 +224,7 @@ public class Run_Abfall {
 
 	}
 
-	private static CarrierScoringFunctionFactoryImpl createMyScoringFunction2 (final Scenario scenario) {
+	private static CarrierScoringFunctionFactoryImpl createMyScoringFunction2(final Scenario scenario) {
 
 		return new CarrierScoringFunctionFactoryImpl(scenario.getNetwork());
 //		return new CarrierScoringFunctionFactoryImpl (scenario, scenario.getConfig().controler().getOutputDirectory()) {
@@ -252,16 +251,4 @@ public class Run_Abfall {
 //			}
 //		};
 	}
-
-//shapfile qgis, Netzwerk aus Link, shipments
-
-
-	//Ausgaben		
-
-	//	System.out.println("Nutzerdaten: "+typLkw1.getUserData());
-	//	System.out.println(typLkw1.getCapacityDimensions());
-	//	System.out.println(typLkw1.toString());	
 }
-
-
-
