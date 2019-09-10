1af03fad7775e3759bc0adf43b8889676f14151e
leich <leich@vsp.tu-berlin.de>
2019-09-10 20:20:39 +0200

    modify Network to updated TransitSchedule and validate

--- src/main/java/org/matsim/prepare/transit/schedule/RunGTFS2MATSimOpenBerlin.java
+++ src/main/java/org/matsim/prepare/transit/schedule/RunGTFS2MATSimOpenBerlin.java
@@ -24,10 +24,18 @@ package org.matsim.prepare.transit.schedule;
 
 import java.time.LocalDate;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
 import org.matsim.contrib.gtfs.RunGTFS2MATSim;
 import org.matsim.contrib.gtfs.TransitSchedulePostProcessTools;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.filter.NetworkFilterManager;
+import org.matsim.core.network.filter.NetworkLinkFilter;
+import org.matsim.core.network.filter.NetworkNodeFilter;
+import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.network.io.NetworkWriter;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
@@ -36,6 +44,8 @@ import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.pt.transitSchedule.api.TransitScheduleWriter;
 import org.matsim.pt.utils.CreatePseudoNetwork;
 import org.matsim.pt.utils.CreateVehiclesForSchedule;
+import org.matsim.pt.utils.TransitScheduleValidator;
+import org.matsim.pt.utils.TransitScheduleValidator.ValidationResult;
 import org.matsim.vehicles.VehicleWriterV1;
 
 /**
@@ -47,6 +57,7 @@ import org.matsim.vehicles.VehicleWriterV1;
 
 public class RunGTFS2MATSimOpenBerlin {
 
+	private static final Logger log = Logger.getLogger(RunGTFS2MATSimOpenBerlin.class);
 	
 	public static void main(String[] args) {
 	
@@ -79,17 +90,48 @@ public class RunGTFS2MATSimOpenBerlin {
 		TransitSchedulePostProcessTools.copyLateDeparturesToStartOfDay(scenario.getTransitSchedule(), 22 * 3600, "copied", false);
 		TransitSchedulePostProcessTools.copyEarlyDeparturesToFollowingNight(scenario.getTransitSchedule(), 6 * 3600, "copied");
 		
-		//if neccessary, parse in an existing network file here:
-//		new MatsimNetworkReader(scenario.getNetwork()).readFile("network.xml");
+		//if necessary, parse in an existing network file here:
+		new MatsimNetworkReader(scenario.getNetwork()).readFile("/home/gregor/git/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-10pct/input/berlin-v5-network.xml.gz");
+		
+		//remove existing pt network (nodes and links)
+		NetworkFilterManager nfmPT = new NetworkFilterManager(scenario.getNetwork());
+		nfmPT.addLinkFilter(new NetworkLinkFilter() {
+			
+			@Override
+			public boolean judgeLink(Link l) {
+				if (l.getId().toString().contains("pt_")) return false;
+				else return true;
+			}
+		});
+		nfmPT.addNodeFilter(new NetworkNodeFilter() {
+
+			@Override
+			public boolean judgeNode(Node n) {
+				if (n.getId().toString().contains("pt_")) return false;
+				else return true;
+			}
+		});
+		Network networkWoPt = nfmPT.applyFilters();
+		new NetworkWriter(networkWoPt).write(networkFile + "_network_filtered_woNewPt.xml.gz");
 		
 		//Create a network around the schedule
-		new CreatePseudoNetwork(scenario.getTransitSchedule(),scenario.getNetwork(),"pt_").createNetwork();
+		new CreatePseudoNetwork(scenario.getTransitSchedule(),networkWoPt,"pt_").createNetwork();
 		
 		//Create simple transit vehicles
 		new CreateVehiclesForSchedule(scenario.getTransitSchedule(), scenario.getTransitVehicles()).run();
 		
+		//Check schedule and network
+		ValidationResult checkResult = TransitScheduleValidator.validateAll(scenario.getTransitSchedule(), networkWoPt);
+		if (checkResult.isValid()) {
+			log.info("TransitSchedule and Network valid according to TransitScheduleValidator");
+			log.warn("TransitScheduleValidator warnings:\n" + checkResult.getWarnings());
+		} else {
+			log.error(checkResult.getErrors());
+			throw new RuntimeException("TransitSchedule and/or Network invalid");
+		}
+		
 		//Write out network, vehicles and schedule
-		new NetworkWriter(scenario.getNetwork()).write(networkFile);
+		new NetworkWriter(networkWoPt).write(networkFile);
 		new TransitScheduleWriter(scenario.getTransitSchedule()).writeFile(scheduleFile);
 		new VehicleWriterV1(scenario.getTransitVehicles()).writeFile(transitVehiclesFile);
 		
