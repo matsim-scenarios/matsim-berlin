844674da7a4bf08af4ab45a9ae470d68e48e7da9
leich <leich@vsp.tu-berlin.de>
2019-10-07 19:04:51 +0200

    comment out uncompiling class

--- src/main/java/org/matsim/analysis/RunBerlinAccidents.java
+++ src/main/java/org/matsim/analysis/RunBerlinAccidents.java
@@ -8,41 +8,41 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.run.RunBerlinScenario;
 
-public class RunBerlinAccidents {
-	
-	private static final Logger log = Logger.getLogger(RunBerlinAccidents.class);
-	
-	public static void main(String[] args) { 
-	
-		for (String arg : args) {
-			log.info( arg );
-		}
-		
-		if ( args.length==0 ) {
-			args = new String[] {"scenarios/berlin-v5.5-1pct/input/berlin-v5.5-1pct.config.xml"}  ;
-		}
-		
-	String outputFile = "/../../shared-svn/studies/countries/de/accidents/data/output/"; 
-	String landOSMInputShapeFile = "/../../shared-svn/studies/countries/de/accidents/data/input/osmBerlin/gis.osm_landuse_a_free_1.shx";  
-	String placesOSMInputFile = "/../../shared-svn/studies/countries/de/accidents/data/input/osmBerlinBrandenburg/gis.osm_landuse_a_free_1_GK4.shx";
-//	String tunnelLinkCSVInputFile = "/../../shared-svn/studies/countries/de/accidents/data/input/tunnellinks.csv";
-//	String planFreeLinkCSVInputFile = "/../../shared-svn/studies/countries/de/accidents/data/input/planfreelinks.csv";
-	
-	
-	Config config = RunBerlinScenario.prepareConfig( args );
-	Scenario scenario = RunBerlinScenario.prepareScenario(config);
-	Controler controler = RunBerlinScenario.prepareControler(scenario);
-	
-	config.controler().setOutputDirectory(outputFile);
-
-	AccidentsConfigGroup accidentsConfig = ConfigUtils.addOrGetModule(config, AccidentsConfigGroup.class);
-	accidentsConfig.setSampleSize(10);
-	accidentsConfig.setLanduseOSMInputShapeFile(landOSMInputShapeFile);
-	accidentsConfig.setPlacesOSMInputFile(placesOSMInputFile);
-//	accidentsConfig.setTunnelLinkCSVInputFile(tunnelLinkCSVInputFile);
-//	accidentsConfig.setPlanFreeLinkCSVInputFile(planFreeLinkCSVInputFile);
-	controler.run();
-	
-	
-	}
-}
+//public class RunBerlinAccidents {
+//	
+//	private static final Logger log = Logger.getLogger(RunBerlinAccidents.class);
+//	
+//	public static void main(String[] args) { 
+//	
+//		for (String arg : args) {
+//			log.info( arg );
+//		}
+//		
+//		if ( args.length==0 ) {
+//			args = new String[] {"scenarios/berlin-v5.5-1pct/input/berlin-v5.5-1pct.config.xml"}  ;
+//		}
+//		
+//	String outputFile = "/../../shared-svn/studies/countries/de/accidents/data/output/"; 
+//	String landOSMInputShapeFile = "/../../shared-svn/studies/countries/de/accidents/data/input/osmBerlin/gis.osm_landuse_a_free_1.shx";  
+//	String placesOSMInputFile = "/../../shared-svn/studies/countries/de/accidents/data/input/osmBerlinBrandenburg/gis.osm_landuse_a_free_1_GK4.shx";
+////	String tunnelLinkCSVInputFile = "/../../shared-svn/studies/countries/de/accidents/data/input/tunnellinks.csv";
+////	String planFreeLinkCSVInputFile = "/../../shared-svn/studies/countries/de/accidents/data/input/planfreelinks.csv";
+//	
+//	
+//	Config config = RunBerlinScenario.prepareConfig( args );
+//	Scenario scenario = RunBerlinScenario.prepareScenario(config);
+//	Controler controler = RunBerlinScenario.prepareControler(scenario);
+//	
+//	config.controler().setOutputDirectory(outputFile);
+//
+//	AccidentsConfigGroup accidentsConfig = ConfigUtils.addOrGetModule(config, AccidentsConfigGroup.class);
+//	accidentsConfig.setSampleSize(10);
+//	accidentsConfig.setLanduseOSMInputShapeFile(landOSMInputShapeFile);
+//	accidentsConfig.setPlacesOSMInputFile(placesOSMInputFile);
+////	accidentsConfig.setTunnelLinkCSVInputFile(tunnelLinkCSVInputFile);
+////	accidentsConfig.setPlanFreeLinkCSVInputFile(planFreeLinkCSVInputFile);
+//	controler.run();
+//	
+//	
+//	}
+//}
