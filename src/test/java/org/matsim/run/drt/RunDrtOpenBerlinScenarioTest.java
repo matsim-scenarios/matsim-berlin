96d8364fe7595b6192feb81bf797daec4ca3911f
leich <leich@vsp.tu-berlin.de>
2019-10-16 19:46:28 +0200

    create test for berlkoenig test agent, use real plans in b. config file

--- src/test/java/org/matsim/run/drt/RunDrtOpenBerlinScenarioTest.java
+++ src/test/java/org/matsim/run/drt/RunDrtOpenBerlinScenarioTest.java
@@ -72,4 +72,48 @@ public class RunDrtOpenBerlinScenarioTest {
 			throw new RuntimeException(ee) ;
 		}
 	}
+	
+	// During debug some exceptions only occured at the replanning stage of the 3rd
+	// iteration, so we need at least 3 iterations.
+	// Have at least 0.1 pct of the population to have as many strange corner cases
+	// as possible (because those tend to cause exceptions otherwise not found).
+	@Test
+	public final void testAFewAgentsOnly() {
+		try {
+			final String[] args = {"scenarios/berlin-v5.5-1pct/input/drt/berlin-drtA-v5.5-1pct-Berlkoenig.config.xml"};
+			
+			Config config = RunDrtOpenBerlinScenario.prepareConfig( args ) ;
+			config.controler().setLastIteration(2);
+			config.strategy().setFractionOfIterationsToDisableInnovation(1);
+			config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+			config.controler().setOutputDirectory( utils.getOutputDirectory() );
+			config.plans().setInputFile("../../../../test/input/drt/BerlkoenigAreaTestAgents.xml");
+			
+			// jvm on build server has less cores than we set in the input config file and would complain about that
+			config.global().setNumberOfThreads(1);
+			config.qsim().setNumberOfThreads(1);
+			
+			for (DrtConfigGroup drtCfg : MultiModeDrtConfigGroup.get(config).getModalElements()) {
+				drtCfg.setNumberOfThreads(1);
+			}
+			
+			Scenario scenario = RunDrtOpenBerlinScenario.prepareScenario( config ) ;
+			// Decrease population to 0.1% sample 
+			List<Id<Person>> agentsToRemove = new ArrayList<>();
+			for (Id<Person> id: scenario.getPopulation().getPersons().keySet()) {
+				if (MatsimRandom.getRandom().nextDouble() < 0.1) {agentsToRemove.add(id);}
+			}
+			for (Id<Person> id: agentsToRemove) {
+				scenario.getPopulation().removePerson(id);
+			}
+			
+			Controler controler = RunDrtOpenBerlinScenario.prepareControler( scenario ) ;
+			
+			controler.run() ;			
+			
+			// TODO: test the scores in iteration 0 and 4
+		} catch ( Exception ee ) {
+			throw new RuntimeException(ee) ;
+		}
+	}
 }
