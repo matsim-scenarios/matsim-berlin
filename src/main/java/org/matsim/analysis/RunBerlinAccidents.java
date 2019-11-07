e2629cdfd5b3c56efcf39a7929d364352cb60854
hacas <hacas@DESKTOP-OFHCER7>
2019-11-07 15:03:26 +0100

    Pfad were corrected so it follow the convention

--- src/main/java/org/matsim/analysis/RunBerlinAccidents.java
+++ src/main/java/org/matsim/analysis/RunBerlinAccidents.java
@@ -35,9 +35,9 @@ public class RunBerlinAccidents {
 			args = new String[] {"scenarios/berlin-v5.5-1pct/input/berlin-v5.5-1pct.config.xml"}  ;
 		}
 		
-	String outputFile = "/../shared-svn/studies/countries/de/accidents/data/output/"; 
-	String landOSMInputShapeFile = "/../shared-svn/studies/countries/de/accidents/data/input/osmBerlin/gis.osm_landuse_a_free_1.shx";  
-	String placesOSMInputFile = "/../shared-svn/studies/countries/de/accidents/data/input/osmBerlinBrandenburg/gis.osm_landuse_a_free_1_GK4.shx";
+	String outputFile = "/../../shared-svn/studies/countries/de/accidents/data/output/"; 
+	String landOSMInputShapeFile = "/../../shared-svn/studies/countries/de/accidents/data/input/osmBerlin/gis.osm_landuse_a_free_1.shx";  
+	String placesOSMInputFile = "/../../shared-svn/studies/countries/de/accidents/data/input/osmBerlinBrandenburg/gis.osm_landuse_a_free_1_GK4.shx";
 	
 	String tunnelLinkCSVInputFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.1.tunnel-linkIDs.csv";
 	//the CSV has also a column for the percentage of the links wich is tunnel, The new String is produced by a new class
