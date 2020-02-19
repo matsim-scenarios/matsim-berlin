0251c8a0152c269e02f9bac6ffb685ef940109f4
leich <leich@vsp.tu-berlin.de>
2020-02-19 17:35:27 +0100

    copyright

--- src/test/java/org/matsim/run/drt/ptRoutingModes/RunDrtOpenBerlinScenarioWithPtRoutingModesTest.java
+++ src/test/java/org/matsim/run/drt/ptRoutingModes/RunDrtOpenBerlinScenarioWithPtRoutingModesTest.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2020 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.run.drt.ptRoutingModes;
 
 import java.util.HashMap;
@@ -64,7 +83,7 @@ public class RunDrtOpenBerlinScenarioWithPtRoutingModesTest {
 			config.plans().setInputFile("../../../../test/input/drt/drt-test-agents.xml");
 			
 			// jvm on build server has less cores than we set in the input config file and would complain about that
-			config.global().setNumberOfThreads(1);
+			config.global().setNumberOfThreads(10);
 			config.qsim().setNumberOfThreads(1);
 			
 			config.controler().setWritePlansInterval(1);
