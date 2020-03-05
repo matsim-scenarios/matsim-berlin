81feb41362de20861a3b31b970b5d3779ab73d40
kainagel <nagel@vsp.tu-berlin.de>
2020-03-05 18:35:38 +0100

    add reference to where the cemdap code sits

---
+++ src/main/java/org/matsim/prepare/cadyts/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The cemdap stuff is under {@link playground.vsp.openberlinscenario.cemdap}.  Since the plan is to replace it by the Actitopp approach, we leave it there.
+ * kai/ihab, mat'20
+ */
+package org.matsim.prepare.cadyts;
