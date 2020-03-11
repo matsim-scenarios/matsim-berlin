d248c860ed81f0451a89edf2b76ef8c41a96c138
Sebastian Mueller <mueller@vsp.tu-berlin.de>
2020-03-11 14:05:21 +0100

    prepare for command line arguments

--- src/main/java/org/matsim/run/drt/KNEventsInfection.java
+++ src/main/java/org/matsim/run/drt/KNEventsInfection.java
@@ -1,5 +1,7 @@
 package org.matsim.run.drt;
 
+import java.util.Arrays;
+
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -20,37 +22,40 @@ class KNEventsInfection{
                         hasCommandLineArgs=false;
                 }
                 if ( hasCommandLineArgs ) {
-                        throw new RuntimeException( "cannot deal with command line args for time being." );
+//                        throw new RuntimeException( "cannot deal with command line args for time being." );
                 }
-
                 Config config = ConfigUtils.createConfig( new EpisimConfigGroup() );
                 EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule( config, EpisimConfigGroup.class );
-                episimConfig.setCase( EpisimConfigGroup.Case.snz25pct );
+//                episimConfig.setCase( EpisimConfigGroup.Case.snz25pct );
+                episimConfig.setInputEventsFile("../snzDrt220.0.events.reduced.xml.gz");
                 episimConfig.setSample(0.25);
-
+                episimConfig.setUsePt(EpisimConfigGroup.UsePt.yes);
+                ConfigUtils.applyCommandline( config, Arrays.copyOfRange( args, 0, args.length ) ) ;
                 
-        	String filename;
-                switch( episimConfig.getCase() ) {
-                        case berlin1pct:
-                                filename = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-1pct/output-berlin-v5.4-1pct/berlin-v5.4-1pct.output_events_wo_linkEnterLeave.xml.gz";
-                                break;
-                        case berlin10pct:
-                                filename = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-10pct/output-berlin-v5.4-10pct/berlin-v5.4-10pct.output_events_reduced.xml.gz";
-                                break;
-                        case snz25pct:
-                            	filename = "../snzDrt220.0.events.reduced.xml.gz";
-                            	break;
-                        default:
-                                throw new IllegalStateException( "Unexpected value: " + episimConfig.getCase() );
-                }
+                
+                
+//        	String filename;
+//                switch( episimConfig.getCase() ) {
+//                        case berlin1pct:
+//                                filename = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-1pct/output-berlin-v5.4-1pct/berlin-v5.4-1pct.output_events_wo_linkEnterLeave.xml.gz";
+//                                break;
+//                        case berlin10pct:
+//                                filename = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-10pct/output-berlin-v5.4-10pct/berlin-v5.4-10pct.output_events_reduced.xml.gz";
+//                                break;
+//                        case snz25pct:
+//                            	filename = "../snzDrt220.0.events.reduced.xml.gz";
+//                            	break;
+//                        default:
+//                                throw new IllegalStateException( "Unexpected value: " + episimConfig.getCase() );
+//                }
 
                 EventsManager events = EventsUtils.createEventsManager();
                 
-                events.addHandler( new InfectionEventHandler() );
+                events.addHandler( new InfectionEventHandler( episimConfig ) );
                 ControlerUtils.checkConfigConsistencyAndWriteToLog(config, "Just before starting iterations");
                 for ( int iteration=0 ; iteration<=1000 ; iteration++ ){
                         events.resetHandlers( iteration );
-                        EventsUtils.readEvents( events, filename );
+                        EventsUtils.readEvents( events, episimConfig.getInputEventsFile() );
                 }
 
         }
