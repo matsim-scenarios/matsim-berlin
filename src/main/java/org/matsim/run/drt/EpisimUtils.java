823e15e1af002455baa4b3f26ef159933d85c030
Sebastian Mueller <mueller@vsp.tu-berlin.de>
2020-03-17 16:48:32 +0100

    activityRelevantForInfectionDynamics & tripRelevantForInfectionDynamics

--- src/main/java/org/matsim/run/drt/EpisimUtils.java
+++ src/main/java/org/matsim/run/drt/EpisimUtils.java
@@ -1,5 +1,7 @@
 package org.matsim.run.drt;
 
+import org.jfree.util.Log;
+
 class EpisimUtils{
         private static double lastNow = -1 ;
         static double getCorrectedTime( double time, long iteration ) {
@@ -25,4 +27,41 @@ class EpisimUtils{
                                 throw new IllegalStateException( "Unexpected value: " + personWrapper.getStatus() );
                 }
         }
+        static boolean activityRelevantForInfectionDynamics( EpisimPerson person, EpisimConfigGroup episimConfig, int iteration) {
+        	boolean actRelevant = true;
+        	String act = person.getTrajectory().get(person.getCurrentPositionInTrajectory());
+        	if (episimConfig.getClosedActivity1() != null) {
+            	if ( act.contains( episimConfig.getClosedActivity1() )&& episimConfig.getClosedActivity1Date() <= iteration) {
+            			actRelevant = false;
+                    }
+            }
+        	if (episimConfig.getClosedActivity2() != null ) {
+            	if ( act.contains( episimConfig.getClosedActivity2() ) && episimConfig.getClosedActivity2Date() <= iteration) {
+            			actRelevant = false;
+                    }
+            }
+        	return actRelevant;
+        }
+		public static boolean tripRelevantForInfectionDynamics(EpisimPerson person, EpisimConfigGroup episimConfig, int iteration) {
+			boolean tripRelevant = true;
+			String lastAct = "";
+			if (person.getCurrentPositionInTrajectory() != 0) {
+				lastAct = person.getTrajectory().get(person.getCurrentPositionInTrajectory()-1); 
+				 
+			} 
+			String nextAct = person.getTrajectory().get(person.getCurrentPositionInTrajectory());
+			
+			if (episimConfig.getClosedActivity1() != null && episimConfig.getClosedActivity1Date() <= iteration) {
+	            if (lastAct.contains(episimConfig.getClosedActivity1()) || nextAct.contains(episimConfig.getClosedActivity1())) {
+	            	tripRelevant = false;
+	            }
+			}
+			if (episimConfig.getClosedActivity2() != null && episimConfig.getClosedActivity2Date() <= iteration) {
+	            if (lastAct.contains(episimConfig.getClosedActivity2()) || nextAct.contains(episimConfig.getClosedActivity2())) {
+	            	tripRelevant = false;
+	            }
+			}
+
+			return tripRelevant;
+		}
 }
