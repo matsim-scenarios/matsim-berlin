730f4d8662d2c0ff626b38041997a5cdff488f3a
hacas <hacas@DESKTOP-OFHCER7>
2020-03-18 12:44:03 +0100

    Running version for cluster and merge with Matsim-Berlin

--- src/main/java/org/matsim/analysis/RunBerlinAccidents.java
+++ src/main/java/org/matsim/analysis/RunBerlinAccidents.java
@@ -9,6 +9,7 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.run.RunBerlinScenario;
 
 /**
@@ -27,31 +28,31 @@ public class RunBerlinAccidents {
 		}
 		
 		if ( args.length==0 ) {
- 			args = new String[] {"scenarios/berlin-v5.5-1pct/input/berlin-v5.5-1pct.config.xml"}  ;
+ 			args = new String[] {"scenarios/berlin-v5.5-10pct/input/berlin-v5.5-10pct.config.xml"}  ;
 		}
 		
-	String outputFile = "./scenarios/berlin-v5.5-1pct/output-berlin-v5.5-1pct-accidents"; 
+	String outputFile = "scenarios/berlin-v5.5-10pct/output-berlin-v5.5-10pct-accidents/"; 
 	String BVWPNetwork = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-network-with-bvwp-accidents-attributes.xml.gz";
-	String plans = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-1pct/input/berlin-v5.5-1pct.plans_uncalibrated.xml.gz";
-	
+	String plans = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-10pct.plans.xml.gz";
+
 	Config config = RunBerlinScenario.prepareConfig(args);
 	config.controler().setOutputDirectory(outputFile);
 	config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
-	config.controler().setLastIteration(0);
+	config.controler().setLastIteration(500);
 	config.plans().setInputFile(plans);
  
 	AccidentsConfigGroup accidentsSettings = ConfigUtils.addOrGetModule(config, AccidentsConfigGroup.class);
 	accidentsSettings.setEnableAccidentsModule(true);
-	accidentsSettings.setScaleFactor(100);
+	accidentsSettings.setScaleFactor(10);
     config.network().setInputFile(BVWPNetwork);
-	
+    config.planCalcScore().getModes().get("car").setMonetaryDistanceRate(-0.0004);
+
 	Scenario scenario = RunBerlinScenario.prepareScenario(config);
-	
+
 	Controler controler = RunBerlinScenario.prepareControler(scenario);
 	controler.addOverridingModule(new AccidentsModule());
-	
-	controler.run();	
+
+	controler.run();
 	}
-	
 }
 
