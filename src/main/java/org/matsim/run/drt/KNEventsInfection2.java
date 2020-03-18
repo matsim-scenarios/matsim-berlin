31a725cc87a546b4467c9f22ed976c80a23a3579
kainagel <nagel@vsp.tu-berlin.de>
2020-03-18 08:42:21 +0100

    change in kai script

--- src/main/java/org/matsim/run/drt/KNEventsInfection2.java
+++ src/main/java/org/matsim/run/drt/KNEventsInfection2.java
@@ -6,6 +6,7 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.ControlerUtils;
 import org.matsim.core.controler.OutputDirectoryLogging;
 import org.matsim.core.events.EventsUtils;
+import scala.Int;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -23,13 +24,30 @@ class KNEventsInfection2{
                 episimConfig.setSample(0.01);
                 episimConfig.setCalibrationParameter(2);
 
-                episimConfig.setUsePt( EpisimConfigGroup.UsePt.no );
-                episimConfig.setUsePtDate( 10. );
-                config.controler().setOutputDirectory( "output-wo-pt-from-it10" );
+//                config.controler().setOutputDirectory( "output-base" );
 
-//                episimConfig.setShutdownEverythingExceptContaining( "home" );
-//                episimConfig.setShutdownDate( 10 );
-//                config.controler().setOutputDirectory( "output-shutdown-everything-except-home-from-it10" );
+//                episimConfig.setUsePt( EpisimConfigGroup.UsePt.no );
+//                episimConfig.setUsePtDate( 10. );
+//                config.controler().setOutputDirectory( "output-wo-pt-from-it10" );
+
+//                episimConfig.setShutdownDate( 30 );
+//                config.controler().setOutputDirectory( "output-shutdown-everything-except-home-from-it30" );
+
+//                int closingIteration = 30 ;
+//                episimConfig.setClosedActivity1( "work" );
+//                episimConfig.setClosedActivity2( "leisure" );
+//                episimConfig.setClosedActivity1Date( closingIteration );
+//                episimConfig.setClosedActivity2Date( closingIteration );
+//                config.controler().setOutputDirectory( "output-shutdown-work-leisure-from-it" + closingIteration );
+
+                int closingIteration = 30 ;
+                episimConfig.setClosedActivity1( "work" );
+                episimConfig.setClosedActivity2( "leisure" );
+                episimConfig.setClosedActivity1Date( closingIteration );
+                episimConfig.setClosedActivity2Date( closingIteration );
+                episimConfig.setUsePt( EpisimConfigGroup.UsePt.no );
+                episimConfig.setUsePtDate( closingIteration );
+                config.controler().setOutputDirectory( "output-shutdown-work-leisure-pt-from-it" + closingIteration );
 
                 ConfigUtils.applyCommandline( config, Arrays.copyOfRange( args, 0, args.length ) ) ;
 
