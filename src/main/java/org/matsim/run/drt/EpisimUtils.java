08cc8c82354c2b490231a7d76c3cabd9aad23b83
kainagel <nagel@vsp.tu-berlin.de>
2020-03-22 17:26:43 +0100

    now it seems to be working again.  must have changed a sign convention without noticing

--- src/main/java/org/matsim/run/drt/EpisimUtils.java
+++ src/main/java/org/matsim/run/drt/EpisimUtils.java
@@ -1,5 +1,6 @@
 package org.matsim.run.drt;
 
+import org.apache.log4j.Logger;
 import org.matsim.run.drt.InfectionEventHandler.EpisimFacility;
 import org.matsim.run.drt.InfectionEventHandler.EpisimVehicle;
 
@@ -8,6 +9,7 @@ import java.util.Random;
 import static org.matsim.run.drt.InfectionEventHandler.*;
 
 class EpisimUtils{
+        private static final Logger log = Logger.getLogger( EpisimUtils.class );
         private static double lastNow = -1 ;
         static double getCorrectedTime( double time, long iteration ) {
                 final double now = Math.min( time, 3600. * 24 ) + iteration * 24. * 3600;
@@ -17,8 +19,8 @@ class EpisimUtils{
                 lastNow = now;
                 return now;
         }
-        /** @noinspection BooleanMethodIsAlwaysInverted*/
-        static boolean hasStatusRelevantForInfectionDynamics( EpisimPerson personWrapper ){
+        /** */
+        private static boolean hasStatusRelevantForInfectionDynamics( EpisimPerson personWrapper ){
                 switch( personWrapper.getDiseaseStatus() ) {
                         case susceptible:
                                 return true;
@@ -36,7 +38,7 @@ class EpisimUtils{
                                 throw new IllegalStateException( "Unexpected value: " + personWrapper.getDiseaseStatus() );
                 }
         }
-        static boolean activityRelevantForInfectionDynamics( EpisimPerson person, EpisimConfigGroup episimConfig, int iteration, Random rnd ) {
+        private static boolean activityRelevantForInfectionDynamics( EpisimPerson person, EpisimConfigGroup episimConfig, int iteration, Random rnd ) {
                 boolean actRelevant = true;
                 String act = person.getTrajectory().get(person.getCurrentPositionInTrajectory());
                 /*
@@ -98,7 +100,7 @@ class EpisimUtils{
                 }
                 return actRelevant;
         }
-        static boolean tripRelevantForInfectionDynamics( EpisimPerson person, EpisimConfigGroup episimConfig, int iteration, Random rnd ) {
+        private static boolean tripRelevantForInfectionDynamics( EpisimPerson person, EpisimConfigGroup episimConfig, int iteration, Random rnd ) {
 //                boolean tripRelevant = true;
                 String lastAct = "";
                 if (person.getCurrentPositionInTrajectory() != 0) {
@@ -155,18 +157,18 @@ class EpisimUtils{
 
         static boolean isRelevantForInfectionDynamics( EpisimPerson personLeavingContainer, EpisimContainer<?> container,
                                                        EpisimConfigGroup episimConfig, int iteration, Random rnd ){
-                if ( !hasStatusRelevantForInfectionDynamics( personLeavingContainer ) || personLeavingContainer.getQuarantineStatus() == QuarantineStatus.full ) {
+                if ( !hasStatusRelevantForInfectionDynamics( personLeavingContainer ) ) {
+                        return false ;
+                }
+                if ( personLeavingContainer.getQuarantineStatus() == QuarantineStatus.full ) {
+                        return false ;
+                }
+                if (container instanceof EpisimFacility && activityRelevantForInfectionDynamics(personLeavingContainer, episimConfig, iteration, rnd )){
                         return true;
                 }
-                if (container instanceof EpisimFacility && !activityRelevantForInfectionDynamics(personLeavingContainer, episimConfig, iteration, rnd )) {
-                        return true;
+                if ( container instanceof EpisimVehicle && tripRelevantForInfectionDynamics(personLeavingContainer, episimConfig, iteration, rnd )) {
+                        return true ;
                 }
-                if (container instanceof EpisimVehicle && !tripRelevantForInfectionDynamics(personLeavingContainer, episimConfig, iteration, rnd )) {
-                        return true;
-                }
-//                if (iteration >= episimConfig.getShutdownDate() && EpisimUtils.isRelevantForShutdown(personLeavingContainer, episimConfig, container)) {
-//                		return;
-//                }
                 return false;
         }
 }
