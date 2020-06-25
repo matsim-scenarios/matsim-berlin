408e6af638238381fd74f4214a0e0fb3d3442eb8
leich <leich@vsp.tu-berlin.de>
2020-06-25 18:42:33 +0200

    update AccessEgressWalk config settings to matsim-libs changes

--- src/main/java/org/matsim/run/RunBerlinScenario.java
+++ src/main/java/org/matsim/run/RunBerlinScenario.java
@@ -37,6 +37,7 @@ import org.matsim.contrib.drt.routing.DrtRouteFactory;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigGroup;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlansCalcRouteConfigGroup.AccessEgressWalkType;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.config.groups.QSimConfigGroup.TrafficDynamics;
 import org.matsim.core.config.groups.VspExperimentalConfigGroup;
@@ -194,7 +195,7 @@ public final class RunBerlinScenario {
 				
 		// vsp defaults
 		config.vspExperimental().setVspDefaultsCheckingLevel( VspExperimentalConfigGroup.VspDefaultsCheckingLevel.info );
-		config.plansCalcRoute().setInsertingAccessEgressWalk( true );
+		config.plansCalcRoute().setInsertingAccessEgressWalk( AccessEgressWalkType.walkToLink );
 		config.qsim().setUsingTravelTimeCheckInTeleportation( true );
 		config.qsim().setTrafficDynamics( TrafficDynamics.kinematicWaves );
 				
