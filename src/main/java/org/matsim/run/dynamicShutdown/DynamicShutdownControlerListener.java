d374fc31944fe3add5fb6543ec8915246fbcba11
jakobrehmann <jakob.rehmann@gmail.com>
2020-07-06 20:08:17 +0200

    Delete DynamicShutdownControlerListener.java

--- src/main/java/org/matsim/run/dynamicShutdown/DynamicShutdownControlerListener.java
+++
@@ -1,9 +0,0 @@
-package org.matsim.run.dynamicShutdown;
-
-public interface DynamicShutdownControlerListener {
-
-    int getDynamicShutdownIteration();
-
-    boolean isDynamicShutdownInitiated();
-
-}
