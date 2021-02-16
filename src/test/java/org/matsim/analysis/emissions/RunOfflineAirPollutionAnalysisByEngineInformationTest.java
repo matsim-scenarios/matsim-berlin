62276589925c6582d169652b0fa1778101ce1575
ikaddoura <kaddoura@vsp.tu-berlin.de>
2021-02-16 15:22:44 +0100

    unignore test

--- src/test/java/org/matsim/analysis/emissions/RunOfflineAirPollutionAnalysisByEngineInformationTest.java
+++ src/test/java/org/matsim/analysis/emissions/RunOfflineAirPollutionAnalysisByEngineInformationTest.java
@@ -19,7 +19,6 @@
 
 package org.matsim.analysis.emissions;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.testcases.MatsimTestUtils;
@@ -29,14 +28,13 @@ public class RunOfflineAirPollutionAnalysisByEngineInformationTest{
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
 
 	// to run this test an environment variable needs to be set in your IDE and on the server...
-	@Ignore
 	@Test
 	public final void test1() {
 		try {
 			String runDirectory = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-0.1pct/output-berlin-v5.4-0.1pct/";
 			String runId = "berlin-v5.4-0.1pct";
-			String hbefaFileWarm = "https://svn.vsp.tu-berlin.de/repos/public-svn/3507bb3997e5657ab9da76dbedbb13c9b5991d3e/0e73947443d68f95202b71a156b337f7f71604ae/7eff8f308633df1b8ac4d06d05180dd0c5fdf577.enc";
-			String hbefaFileCold = "https://svn.vsp.tu-berlin.de/repos/public-svn/3507bb3997e5657ab9da76dbedbb13c9b5991d3e/0e73947443d68f95202b71a156b337f7f71604ae/22823adc0ee6a0e231f35ae897f7b224a86f3a7a.enc";
+			String hbefaFileWarm = "https://svn.vsp.tu-berlin.de/repos/public-svn/3507bb3997e5657ab9da76dbedbb13c9b5991d3e/0e73947443d68f95202b71a156b337f7f71604ae/f5b276f41a0531ed740a81f4615ec00f4ff7a28d.enc";
+			String hbefaFileCold = "https://svn.vsp.tu-berlin.de/repos/public-svn/3507bb3997e5657ab9da76dbedbb13c9b5991d3e/0e73947443d68f95202b71a156b337f7f71604ae/b63f949211b7c93776cdce8a7600eff4e36460c8.enc";
 			
 			RunOfflineAirPollutionAnalysisByEngineInformation analysis = new RunOfflineAirPollutionAnalysisByEngineInformation(runDirectory, runId, hbefaFileWarm, hbefaFileCold, utils.getOutputDirectory());
 			analysis.run();
