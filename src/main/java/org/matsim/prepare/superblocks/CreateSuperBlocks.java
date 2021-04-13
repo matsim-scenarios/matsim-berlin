23718d68d219642b8adacab4a20e41fe308470d2
tschlenther <schlenther@vsp.tu-berlin.de>
2021-04-13 17:08:09 +0200

    CreateSuperBlocks

---
+++ src/main/java/org/matsim/prepare/superblocks/CreateSuperBlocks.java
@@ -0,0 +1,43 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.prepare.superblocks;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
+
+import java.util.List;
+import java.util.Set;
+
+public class CreateSuperBlocks {
+
+	static void blockLinks(Network network, List<Id<Link>> linksToBlock){
+
+		MultimodalNetworkCleaner cleaner = new MultimodalNetworkCleaner(network);
+		cleaner.run(Set.of(TransportMode.car));
+	}
+
+
+
+}
