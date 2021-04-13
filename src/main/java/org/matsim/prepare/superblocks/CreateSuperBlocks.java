f3dfb3185bb9c688ca8fe5ed4cb1936bc69089e9
tschlenther <schlenther@vsp.tu-berlin.de>
2021-04-13 18:12:04 +0200

    example

--- src/main/java/org/matsim/prepare/superblocks/CreateSuperBlocks.java
+++ src/main/java/org/matsim/prepare/superblocks/CreateSuperBlocks.java
@@ -20,12 +20,16 @@
 
 package org.matsim.prepare.superblocks;
 
+import org.locationtech.jts.geom.prep.PreparedGeometry;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.run.drt.BerlinShpUtils;
+import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 
 import java.util.List;
 import java.util.Set;
@@ -33,6 +37,11 @@ import java.util.Set;
 public class CreateSuperBlocks {
 
 	static void blockLinks(Network network, List<Id<Link>> linksToBlock){
+
+
+		String shpFilePath = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/pave/shp-files/berlin-planungsraum-hundekopf";
+		List<PreparedGeometry> shpFile = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(shpFilePath));
+
 
 		MultimodalNetworkCleaner cleaner = new MultimodalNetworkCleaner(network);
 		cleaner.run(Set.of(TransportMode.car));
