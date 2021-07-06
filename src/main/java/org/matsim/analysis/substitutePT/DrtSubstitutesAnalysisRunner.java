7e5c9685eeaf9addc1513666e456ebdb36d1e621
tschlenther <schlenther@vsp.tu-berlin.de>
2021-07-06 18:05:38 +0200

    towards a runner object type rather than a script

--- src/main/java/org/matsim/analysis/substitutePT/DrtSubstitutesAnalysisRunner.java
+++ src/main/java/org/matsim/analysis/substitutePT/DrtSubstitutesAnalysisRunner.java
@@ -24,11 +24,19 @@ import org.apache.log4j.Logger;
 import org.matsim.analysis.personMoney.PersonMoneyEventsAnalysisModule;
 import org.matsim.analysis.pt.stop2stop.PtStop2StopAnalysisModule;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.accessibility.AccessibilityConfigGroup;
+import org.matsim.contrib.accessibility.Modes4Accessibility;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigGroup;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.utils.io.IOUtils;
 import org.matsim.run.RunBerlinScenario;
 import org.matsim.run.accessibility.RunBerlinScenarioWithAccessibilities;
+import org.matsim.run.drt.RunDrtOpenBerlinScenario;
+
+import java.io.File;
 
 /**
  *
@@ -45,7 +53,7 @@ import org.matsim.run.accessibility.RunBerlinScenarioWithAccessibilities;
  *
  */
 
-//TODO maybe call this *Analysis and move it to the analysis package?
+//TODO restructure this such that it has the structure of a run class (prepareConfig+ prepareScenario + prepareControler) and it configures everything (independently of whether drt is used in the underlying run)
 class DrtSubstitutesAnalysisRunner {
 
 	private static final Logger log = Logger.getLogger(DrtSubstitutesAnalysisRunner.class);
@@ -83,5 +91,45 @@ class DrtSubstitutesAnalysisRunner {
 		});
 
 		controler.run();
+	}
+
+	//TODO allow more custom modules to add?
+	static Config prepareConfigBasedOnOutputConfig(String pathToOutputConfig, boolean drtUsed){
+
+		String[] args = new String[1];
+		args[0] = pathToOutputConfig;
+		Config config;
+		if(drtUsed){
+			config = RunDrtOpenBerlinScenario.prepareConfig(args);
+		} else {
+			ConfigGroup[] customModulesToAdd = new ConfigGroup[]{new AccessibilityConfigGroup()};
+			config = RunBerlinScenario.prepareConfig(args, customModulesToAdd);
+		}
+
+		//now do everything that RunBerlinScenarioWithAccessibilities.prepareConfig does
+		{
+			File opportunitiesFile = new File("../../shared-svn/projects/accessibility-berlin/osm/berlin/amenities/2018-05-30/facilities_classified.xml");
+			config.facilities().setInputFile(opportunitiesFile.getAbsolutePath());
+
+			AccessibilityConfigGroup acg = ConfigUtils.addOrGetModule(config, AccessibilityConfigGroup.class);
+			acg.setTimeOfDay((8*60.+5.)*60.);
+			acg.setAreaOfAccessibilityComputation(AccessibilityConfigGroup.AreaOfAccesssibilityComputation.fromShapeFile);
+			acg.setShapeFileCellBasedAccessibility("../../shared-svn/studies/countries/de/open_berlin_scenario/input/shapefiles/2013/Berlin_DHDN_GK4.shp");
+			acg.setTileSize_m(5000);
+			acg.setComputingAccessibilityForMode(Modes4Accessibility.freespeed, false);
+			acg.setComputingAccessibilityForMode(Modes4Accessibility.car, false);
+			acg.setComputingAccessibilityForMode(Modes4Accessibility.pt, true);
+			acg.setOutputCrs(config.global().getCoordinateSystem());
+		}
+
+
+		config.controler().setLastIteration(0);
+		config.plans().setInputFile(""); //TODO output plans file from the given config (think there is some utility method hidden somewhere for that)
+		config.controler().setOutputDirectory("drtSubstitutesPTAnalysis");//TODO probably will have to be adjusted after this method is called. can only set this such that nothing from the original run will be overwritten
+
+		config.controler().setWriteEventsInterval(0);
+		config.controler().setWritePlansInterval(0);
+		config.controler().setDumpDataAtEnd(false); //TODO check! (idea is to minimize output as much as possible to our specific analysis. but we need trips.csv.gz)
+		return config;
 	}
 }
