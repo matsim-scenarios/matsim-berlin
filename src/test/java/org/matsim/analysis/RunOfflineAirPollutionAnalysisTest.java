fab724c9d0f9025103defc5b0e38c37e4ffb5f1b
leich <leich@vsp.tu-berlin.de>
2021-07-13 16:25:37 +0200

    add comment fixme

--- src/test/java/org/matsim/analysis/RunOfflineAirPollutionAnalysisTest.java
+++ src/test/java/org/matsim/analysis/RunOfflineAirPollutionAnalysisTest.java
@@ -29,6 +29,7 @@ public class RunOfflineAirPollutionAnalysisTest{
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
 
 	// to run this test an environment variable needs to be set in your IDE and on the server...
+	// FIXME
 	@Ignore
 	@Test
 	public final void test1() {
