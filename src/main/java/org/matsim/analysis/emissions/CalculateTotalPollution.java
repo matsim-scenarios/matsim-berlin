c0c67c37e150b9b00335c4c2e1550d70c6e03cbd
anton <web@antonstock.de>
2021-08-24 01:06:34 +0200

    added total pollution calculation (might not work)

---
+++ src/main/java/org/matsim/analysis/emissions/CalculateTotalPollution.java
@@ -0,0 +1,46 @@
+package org.matsim.analysis.emissions;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.emissions.Pollutant;
+import org.matsim.contrib.emissions.events.ColdEmissionEvent;
+import org.matsim.contrib.emissions.events.ColdEmissionEventHandler;
+import org.matsim.contrib.emissions.events.WarmEmissionEvent;
+import org.matsim.contrib.emissions.events.WarmEmissionEventHandler;
+import org.matsim.core.network.NetworkUtils;
+
+import java.util.Map;
+
+/**
+ * by antonstock
+ */
+
+public class CalculateTotalPollution implements WarmEmissionEventHandler, ColdEmissionEventHandler {
+
+    //set the emission type of interest e.g. "CO", "CO2_TOTAL", "NOx" etc.
+    static final String emissionType ="CO2_TOTAL";
+    int counter=0;
+    double emissionCounter=0;
+
+    @Override
+    public void handleEvent (WarmEmissionEvent warmEvent) {
+        for (Map.Entry<Pollutant, Double> pollutant : warmEvent.getWarmEmissions().entrySet()) {
+                var key = pollutant.getKey();
+                var value = pollutant.getValue();
+                counter++;
+                if (emissionType.equals(key.toString())) {
+                    emissionCounter = emissionCounter+value;
+                }
+        }
+    }
+    @Override
+    public void handleEvent (ColdEmissionEvent coldEvent) {
+        for (Map.Entry<Pollutant, Double> pollutant : coldEvent.getColdEmissions().entrySet()) {
+            var key = pollutant.getKey();
+            var value = pollutant.getValue();
+            counter++;
+            if (emissionType.equals(key.toString())) {
+                emissionCounter = emissionCounter+value;
+            }
+        }
+    }
+}
