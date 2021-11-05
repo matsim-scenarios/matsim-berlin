affd361768233b751a518dd178bdd6f5dc04cfaf
Janekdererste <janek.laudan@mailbox.org>
2021-11-05 13:25:44 +0100

    do the whole thing in utm 33

--- src/main/java/org/matsim/run/MosaikRunner.java
+++ src/main/java/org/matsim/run/MosaikRunner.java
@@ -6,6 +6,7 @@ import org.locationtech.jts.geom.prep.PreparedGeometry;
 import org.locationtech.jts.geom.prep.PreparedGeometryFactory;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.NetworkWriter;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.core.config.groups.StrategyConfigGroup;
@@ -14,19 +15,25 @@ import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.replanning.strategies.ReRoute;
 import org.matsim.core.router.TripStructureUtils;
+import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.facilities.ActivityFacilitiesFactoryImpl;
 import org.matsim.vis.snapshotwriters.SnapshotWritersModule;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TransferQueue;
 import java.util.stream.Collectors;
 
 public class MosaikRunner {
 
+    private static final CoordinateTransformation transformation = TransformationFactory.getCoordinateTransformation(TransformationFactory.DHDN_GK4, "EPSG:25833");
+
     public static void main(String[] args) {
 
         var config = RunBerlinScenario.prepareConfig(args);
+        config.global().setCoordinateSystem("EPSG:25833");
         config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
         config.strategy().clearStrategySettings();
 
@@ -60,79 +67,18 @@ public class MosaikRunner {
 
         var scenario = RunBerlinScenario.prepareScenario(config);
 
-        /*
-        // merge transit schedule and network
-
-        // remove all references of the old network from the schedule
-        for (TransitStopFacility facility : scenario.getTransitSchedule().getFacilities().values()) {
-            facility.setLinkId(null);
-        }
-
-        // remove network routes from schedule
-        scenario.getTransitSchedule().getTransitLines().values().parallelStream()
-                .flatMap(transitLine -> transitLine.getRoutes().values().stream())
-                .forEach(route -> route.setRoute(null));
-
-        // remove all facilities from transit schedule which were added by a previous run of create pseudo network this is quite brittle and relies on the implementation of createpseudonetwork
-        scenario.getTransitSchedule().getFacilities().values().stream()
-                .filter(f -> f.getId().toString().contains("."))
-                .collect(Collectors.toSet())
-                .forEach(stop -> scenario.getTransitSchedule().removeStopFacility(stop));
-
-        // add pseudo network for pt
-        new CreatePseudoNetwork(scenario.getTransitSchedule(), scenario.getNetwork(), "pt_", 0.1, 100000.0).createNetwork();
-
-         */
-
-        var bbox = createBoundingBox();
-        var network = scenario.getNetwork();
-        // if links are within the bounding box replace simplified links with detailed geometries
-        // first add detailed geometries for the original geometry information
-        var linksToDelete = network.getLinks().values().parallelStream()
-                .filter(link -> isCoveredBy(link, bbox))
-                .peek(link -> {
-
-                    // get the original geometry information from the link attributes
-                    var originalGeometry = NetworkUtils.getOriginalGeometry(link);
-
-                    // convert original link's attributes into map once
-                    var attributes = link.getAttributes().getAsMap();
-
-                    // for each node in the original geometry add one link
-                    for (int i = 0; i < originalGeometry.size(); i++) {
-
-                        network.addNode(originalGeometry.get(i));
-
-                        var from = i == 0 ? link.getFromNode() : originalGeometry.get(i);
-                        var to = i == originalGeometry.size() - 1 ? link.getToNode() : originalGeometry.get(i + 1);
-                        var newLink = network.getFactory().createLink(Id.createLinkId(link.getId().toString() + "_" + i), from, to);
-
-                        // copy all values of the original link
-                        newLink.setAllowedModes(link.getAllowedModes());
-                        newLink.setCapacity(link.getCapacity());
-                        newLink.setFreespeed(link.getFreespeed());
-                        newLink.setNumberOfLanes(link.getNumberOfLanes());
-
-                        // copy all unstructured attributes
-                        attributes.forEach((key, value) -> newLink.getAttributes().putAttribute(key, value));
-
-                        network.addLink(newLink);
-                    }
-                })
-                .collect(Collectors.toSet());
-
-        // now delete the links simplified links
-        for (var link : linksToDelete) {
-            network.removeLink(link.getId());
-        }
-
         // remove linkids from activities
+        // transform activity coordinates to utm-33
         scenario.getPopulation().getPersons().values().parallelStream()
                 .flatMap(person -> person.getPlans().stream())
                 .flatMap(plan -> plan.getPlanElements().stream())
                 .filter(element -> element instanceof Activity)
                 .map(element -> (Activity) element)
-                .forEach(activity -> activity.setLinkId(null));
+                .forEach(activity -> {
+                    activity.setLinkId(null);
+                    var transformed = transformation.transform(activity.getCoord());
+                    activity.setCoord(transformed);
+                });
 
         // replace trips in plans with single empty legs which only have a main mode
         for (var person : scenario.getPopulation().getPersons().values()) {
@@ -162,7 +108,6 @@ public class MosaikRunner {
             }
         }
 
-
         // remove linkids from facilities
         // facilities are basically immutable. Create new facilities without link references
         var factory = new ActivityFacilitiesFactoryImpl();
@@ -175,6 +120,8 @@ public class MosaikRunner {
 
         // add new facilities to scenario
         for (var facility : facilities) {
+            var transformed = transformation.transform(facility.getCoord());
+            facility.setCoord(transformed);
             scenario.getActivityFacilities().addActivityFacility(facility);
         }
 
