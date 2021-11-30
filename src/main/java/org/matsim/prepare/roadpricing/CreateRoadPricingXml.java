84598b67cd76523e691c340180799ccbf37c4aca
G. Leich <leich@vsp.tu-berlin.de>
2021-11-30 12:11:51 +0100

    add script for cordon toll

--- src/main/java/org/matsim/prepare/roadpricing/CreateRoadPricingXml.java
+++ src/main/java/org/matsim/prepare/roadpricing/CreateRoadPricingXml.java
@@ -21,7 +21,6 @@ package org.matsim.prepare.roadpricing;
 
 import org.locationtech.jts.geom.prep.PreparedGeometry;
 import org.matsim.api.core.v01.Coord;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.roadpricing.RoadPricingScheme;
@@ -43,17 +42,19 @@ public class CreateRoadPricingXml {
 //        String zoneShpFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/avoev/shp-files/shp-berlin-bezirksregionen/berlin-bezirksregion_GK4_fixed.shp";
         String zoneShpFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/avoev/shp-files/shp-inner-city-area/inner-city-area.shp";
         String zoneShpCRS = "EPSG:31468";
-        String outputFile = "berlin_distance_roadpricing.xml";
+//        String outputFile = "berlin_distance_roadpricing.xml";
+        String outputFile = "berlin_cordon_roadpricing.xml";
         if (args.length == 0) {
             args = new String[]{"scenarios/berlin-v5.5-1pct/input/berlin-v5.5-1pct.config.xml"};
         }
         Config config =  RunBerlinScenario.prepareConfig( args ) ;
         Scenario scenario = RunBerlinScenario.prepareScenario( config ) ;
 
-        createRoadPricingXml(scenario, zoneShpFile, zoneShpCRS, outputFile);
+//        createDistanceCostRoadPricingXml(scenario, zoneShpFile, zoneShpCRS, outputFile);
+        createCordonRoadPricingXml(scenario, zoneShpFile, zoneShpCRS, outputFile);
     }
 
-    static void createRoadPricingXml (Scenario scenario, String zoneShpFile, String shapeFileCRS, String outputFile) {
+    static void createDistanceCostRoadPricingXml(Scenario scenario, String zoneShpFile, String shapeFileCRS, String outputFile) {
         CoordinateTransformation transformer = TransformationFactory.getCoordinateTransformation(scenario.getConfig().global().getCoordinateSystem(), shapeFileCRS);
         List<PreparedGeometry> geometries = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(zoneShpFile));
 
@@ -78,6 +79,36 @@ public class CreateRoadPricingXml {
                 Time.parseTime("00:00:00"),
                 Time.parseTime("30:00:00"),
                 0.00005);
+
+        RoadPricingWriterXMLv1 writer = new RoadPricingWriterXMLv1(scheme);
+        writer.writeFile(outputFile);
+    }
+
+    static void createCordonRoadPricingXml(Scenario scenario, String zoneShpFile, String shapeFileCRS, String outputFile) {
+        CoordinateTransformation transformer = TransformationFactory.getCoordinateTransformation(scenario.getConfig().global().getCoordinateSystem(), shapeFileCRS);
+        List<PreparedGeometry> geometries = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(zoneShpFile));
+
+        RoadPricingSchemeImpl scheme = RoadPricingUtils.addOrGetMutableRoadPricingScheme(scenario );
+
+        /* Configure roadpricing scheme. */
+        RoadPricingUtils.setName(scheme, "Berlin_distance_toll");
+        RoadPricingUtils.setType(scheme, RoadPricingScheme.TOLL_TYPE_LINK);
+        RoadPricingUtils.setDescription(scheme, "cordon toll within Berlin inner city border");
+
+        /* Add general toll. */
+        for (Link link : scenario.getNetwork().getLinks().values()) {
+            Coord fromNodeTransformed = transformer.transform(link.getFromNode().getCoord());
+            Coord toNodeTransformed = transformer.transform(link.getToNode().getCoord());
+            if (!ShpGeometryUtils.isCoordInPreparedGeometries(fromNodeTransformed, geometries) &&
+                    ShpGeometryUtils.isCoordInPreparedGeometries(toNodeTransformed, geometries)) {
+                RoadPricingUtils.addLink(scheme, link.getId());
+            }
+        }
+
+        RoadPricingUtils.createAndAddGeneralCost(scheme,
+                Time.parseTime("00:00:00"),
+                Time.parseTime("30:00:00"),
+                5.0);
 
         RoadPricingWriterXMLv1 writer = new RoadPricingWriterXMLv1(scheme);
         writer.writeFile(outputFile);
