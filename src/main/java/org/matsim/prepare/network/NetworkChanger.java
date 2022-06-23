dd37215d3adbccaf6cb5cf03aaec04d261f3c3e7
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2022-06-23 16:48:12 +0200

    added node ids as comment

--- src/main/java/org/matsim/prepare/network/NetworkChanger.java
+++ src/main/java/org/matsim/prepare/network/NetworkChanger.java
@@ -14,8 +14,10 @@ public class NetworkChanger {
 
 	public static void addA100Extension(Network network) {
 		double length;
-		Node startNode;
-		Node targetNode;
+		Node startNode1;
+		Node startNode2;
+		Node targetNode1;
+		Node targetNode2;
 
 		// set specification for all links
 		Set<String> allowedModes = new HashSet<>();
@@ -34,30 +36,53 @@ public class NetworkChanger {
 		{
 			// Grenzallee to Sonnenallee
 			length = null;  // TODO
-			startNode = null;
-			targetNode = null;
-			addLinkToNetwork(network, startNode, targetNode, allowedModes, capacity, freespeed, length, lanes);
+			startNode1 = null;  // 27542432 nach Norden
+			startNode2 = null;  // 27542414 nach Sueden/Westen
+			targetNode1 = null;  // 31390656 nach Norden
+			targetNode2 = null;  // 1918530791
+			addLinkToNetwork(network, startNode1, targetNode1, allowedModes, capacity, freespeed, length, lanes);
+			addLinkToNetwork(network, targetNode2, startNode2, allowedModes, capacity, freespeed, length, lanes);
 
 			// Sonnenallee to Treptower Park
 			length = null;  // TODO
-			startNode = null;
-			targetNode = null;
-			addLinkToNetwork(network, startNode, targetNode, allowedModes, capacity, freespeed, length, lanes);
+			startNode1 = null;  // 31357356 nach Norden
+			startNode2 = null;  // 31357357 nach Sueden
+			targetNode1 = null;  // 29787110
+			targetNode2 = null;  // 29787103 sueden
+			addLinkToNetwork(network, startNode1, targetNode1, allowedModes, capacity, freespeed, length, lanes);
+			addLinkToNetwork(network, targetNode2, startNode2, allowedModes, capacity, freespeed, length, lanes);
 		}
 
 		// ---- Abschnitt 17 (4.1 km) ----
 		{
 			// Treptower Park to Ostkreuz
 			length = null;  // TODO
-			startNode = null;
-			targetNode = null;
+			startNode1 = null;  // 29787110 nach Norden
+			startNode2 = null;  // 29787103 nach Sueden
+			targetNode1 = null;  // 4317221792    --> Ostkreuz hat nur einen Punkt auf der Markgrafenstr, einen auf Hauptstr
+			targetNode2 = null;  // Hauptstr: 4370346530 nach Osten, 4245068305   nach Westen
 			addLinkToNetwork(network, startNode, targetNode, allowedModes, capacity, freespeed, length, lanes);
+			addLinkToNetwork(network, targetNode, startNode, allowedModes, capacity, freespeed, length, lanes);
 
-			// Ostkreuz to Storkower Strasse
+			// Ostkreuz to Franfurter Allee
 			length = null;  // TODO
-			startNode = null;
-			targetNode = null;
+			startNode1 = null; //
+			startNode2 = null;  //
+			targetNode1 = null;  //  Guertelstr: 1791505417
+			targetNode2 = null;  //  Kreuzung Moellendorffstr/Frankfurter Allee (von unten links gegen den Uhrzeigersinn):
+			// 12614683, 598234402, 288267826, 29784919
 			addLinkToNetwork(network, startNode, targetNode, allowedModes, capacity, freespeed, length, lanes);
+			addLinkToNetwork(network, targetNode, startNode, allowedModes, capacity, freespeed, length, lanes);
+
+
+			// Franfurter Allee to Storkower Strasse (28373619, 28373623)
+			length = null;  // TODO
+			startNode1 = null; //
+			startNode2 = null;  //
+			targetNode1 = null;  //
+			targetNode2 = null;  //
+			addLinkToNetwork(network, startNode, targetNode, allowedModes, capacity, freespeed, length, lanes);
+			addLinkToNetwork(network, targetNode, startNode, allowedModes, capacity, freespeed, length, lanes);
 		}
 	}
 
