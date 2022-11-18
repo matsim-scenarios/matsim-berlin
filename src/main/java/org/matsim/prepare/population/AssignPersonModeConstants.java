6ec6de9edbdfbc5d25efdfaa400e5f89dd665931
vsp-gleich <leich@vsp.tu-berlin.de>
2022-11-18 23:15:59 +0100

    use uniform distribution instead

--- src/main/java/org/matsim/prepare/population/AssignPersonModeConstants.java
+++ src/main/java/org/matsim/prepare/population/AssignPersonModeConstants.java
@@ -25,32 +25,46 @@ import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.population.PersonUtils;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.scoring.functions.PersonSpecificScoringAttributesSetter;
+import org.matsim.utils.math.RandomFromDistribution;
 
-import java.util.Collection;
-import java.util.SplittableRandom;
+import java.util.*;
 
 public class AssignPersonModeConstants {
 
-	private static final Logger log = LogManager.getLogger(AssignPersonModeConstants.class);
+    private static final Logger log = LogManager.getLogger(AssignPersonModeConstants.class);
 
-	public static void main (String[] args) {
-		String inputPopulationFile = "~/git/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-10pct.plans.xml.gz";
-		String mode = TransportMode.drt;
-		double logNormalMean = -0.1;
-		double logNormalSigma = 0.1;
-		String outputPopulationFile = "berlin-v5.5-10pct_drtAscLogNormal_mean_" +
-				logNormalMean + "_sigma_" + logNormalSigma + ".plans.xml.gz";
+    public static void main(String[] args) {
+        String inputPopulationFile = "~/git/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-10pct.plans.xml.gz";
+        String mode = TransportMode.drt;
+        double mean = 0.0;
+        double sigma = 6.0;
+//		String outputPopulationFile = "berlin-v5.5-10pct_drtAscLogNormal_mean_" +
+        String outputPopulationFile = "berlin-v5.5-10pct_drtAscUniform_mean_" +
+                mean + "_sigma_" + sigma + ".plans.xml.gz";
 
-		Population population = PopulationUtils.readPopulation(inputPopulationFile);
+        Population population = PopulationUtils.readPopulation(inputPopulationFile);
 
-		SplittableRandom splittableRandom = new SplittableRandom(1234);
-		PersonSpecificScoringAttributesSetter.setLogNormalModeConstant((Collection<Person>) population.getPersons().values(),
-				mode, logNormalMean, logNormalSigma, splittableRandom);
+//		SplittableRandom splittableRandom = new SplittableRandom(1234);
+//		PersonSpecificScoringAttributesSetter.setLogNormalModeConstant((Collection<Person>) population.getPersons().values(),
+//				mode, mean, sigma, splittableRandom);
 
-		PopulationUtils.writePopulation(population, outputPopulationFile);
-	}
+        Random random = new Random(1234);
+        population.getPersons().values().forEach((person) -> {
+            Map<String, String> modeConstants = PersonUtils.getModeConstants(person);
+            if (modeConstants == null) {
+                modeConstants = new HashMap();
+            }
+
+            modeConstants.put(mode, Double.toString((random.nextDouble() - 0.5) * 2 * sigma + mean));
+            PersonUtils.setModeConstants(person, modeConstants);
+        });
+
+
+        PopulationUtils.writePopulation(population, outputPopulationFile);
+    }
 
 }
 
