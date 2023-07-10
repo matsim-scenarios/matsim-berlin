a8e70f85e78d8e2f61ebf27dd464d52233ec2dcd
hzoerner <h.zoerner@campus.tu-berlin.de>
2023-07-10 17:05:17 +0200

    added map plot

--- src/main/java/org/matsim/dashboard/DTVComparisonDashboard.java
+++ src/main/java/org/matsim/dashboard/DTVComparisonDashboard.java
@@ -1,9 +1,14 @@
 package org.matsim.dashboard;
 
+import org.matsim.application.analysis.traffic.CountComparisonAnalysis;
+import org.matsim.application.prepare.network.CreateGeoJsonNetwork;
 import org.matsim.dashboard.analysis.DTVAnalysis;
 import org.matsim.simwrapper.Dashboard;
 import org.matsim.simwrapper.Header;
 import org.matsim.simwrapper.Layout;
+import org.matsim.simwrapper.dashboard.TrafficCountsDashboard;
+import org.matsim.simwrapper.viz.Bar;
+import org.matsim.simwrapper.viz.MapPlot;
 import org.matsim.simwrapper.viz.PieChart;
 import org.matsim.simwrapper.viz.Table;
 
@@ -20,7 +25,7 @@ public class DTVComparisonDashboard implements Dashboard {
 		header.title = "Daily traffic volume analysis dashboard";
 		header.description = "Analysis of count data source provided by 'FIS-Broker' (Open geo-data portal by Berlin)";
 
-		layout.row("first").el(PieChart.class, (viz, data) -> {
+		layout.row("mapping").el(PieChart.class, (viz, data) -> {
 
 			viz.dataset = data.compute(DTVAnalysis.class, "stations_per_road_type.csv");
 			viz.title = "Mapped count stations per road type";
@@ -32,11 +37,25 @@ public class DTVComparisonDashboard implements Dashboard {
 			viz.title = "Characteristics";
 		});
 
+		layout.row("map").el(MapPlot.class, (viz, data) -> {
+			viz.title = "Estimation quality";
+			viz.height = 8.0D;
+			viz.setShape(data.compute(CreateGeoJsonNetwork.class, "network.geojson", new String[]{"--with-properties"}), "id");
+			viz.addDataset("counts", data.compute(DTVAnalysis.class, "day_quality_per_road_type.csv"));
+			viz.center = data.context().getCenter();
+			viz.zoom = data.context().mapZoomLevel;
+			viz.display.lineWidth.dataset = "@8";
+		});
+
+		layout.row("aggregation").el(Bar.class, (viz, data) -> {
+
+		});
+
 		/*
-		* TODO
-		*  peak hour comparision
-		*  whole day comaprision
-		*  map plot for both comparision?
-		* */
+		 * TODO
+		 *  peak hour comparision
+		 *  whole day comaprision
+		 *  map plot for both comparision?
+		 * */
 	}
 }
