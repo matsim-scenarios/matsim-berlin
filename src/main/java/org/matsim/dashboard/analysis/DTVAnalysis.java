652fd28afe04c1ce838d99293ecc82e3cecd44bc
hzoerner <h.zoerner@campus.tu-berlin.de>
2023-07-10 17:18:31 +0200

    code clean up, analysis

--- src/main/java/org/matsim/dashboard/analysis/DTVAnalysis.java
+++ src/main/java/org/matsim/dashboard/analysis/DTVAnalysis.java
@@ -1,54 +1,50 @@
 package org.matsim.dashboard.analysis;
 
 import it.unimi.dsi.fastutil.ints.IntArrayList;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.poi.ss.formula.Formula;
 import org.matsim.analysis.VolumesAnalyzer;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.application.CommandSpec;
 import org.matsim.application.MATSimAppCommand;
+import org.matsim.application.options.CsvOptions;
 import org.matsim.application.options.InputOptions;
 import org.matsim.application.options.OutputOptions;
 import org.matsim.core.api.experimental.events.EventsManager;
-import org.matsim.core.config.groups.NetworkConfigGroup;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.network.NetworkUtils;
-import org.matsim.core.network.filter.NetworkFilterManager;
-import org.matsim.prepare.counts.CreateCountsFromGeoPortalBerlin;
 import picocli.CommandLine;
+import tech.tablesaw.aggregate.AggregateFunction;
 import tech.tablesaw.aggregate.AggregateFunctions;
+import tech.tablesaw.aggregate.NumericAggregateFunction;
 import tech.tablesaw.api.*;
 import tech.tablesaw.columns.Column;
-import tech.tablesaw.io.DataFrameReader;
-import tech.tablesaw.io.ReaderRegistry;
+import tech.tablesaw.io.csv.CsvReadOptions;
 import tech.tablesaw.selection.Selection;
-import tech.tablesaw.table.Relation;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @CommandLine.Command(name = "dtv", description = "analysis of simulated traffic volumes")
 @CommandSpec(
 		requireEvents = true,
 		requireNetwork = true,
 		requires = "dtv_berlin.csv",
-		produces = {"mapping_overview.csv", "stations_per_road_type.csv", "day_quality_per_road_type.csv"}
+		produces = {"mapping_overview.csv", "stations_per_road_type.csv", "day_quality_per_road_type.csv", "peak_quality_per_road_type.csv", "dtv_comparison.csv", "dtv_valid_and_quality.csv"}
 )
 public class DTVAnalysis implements MATSimAppCommand {
 
 	private static final Logger log = LogManager.getLogger(DTVAnalysis.class);
 
 	@CommandLine.Mixin
-	private final InputOptions input = InputOptions.ofCommand(CreateCountsFromGeoPortalBerlin.class);
+	private final InputOptions input = InputOptions.ofCommand(DTVAnalysis.class);
 
 	@CommandLine.Mixin
-	private final OutputOptions output = OutputOptions.ofCommand(CreateCountsFromGeoPortalBerlin.class);
+	private final OutputOptions output = OutputOptions.ofCommand(DTVAnalysis.class);
 
 	@CommandLine.Option(names = "--upper-limit", description = "upper limit for 'exact' estimation", defaultValue = "1.3")
 	private double upperLimit;
@@ -56,32 +52,26 @@ public class DTVAnalysis implements MATSimAppCommand {
 	@CommandLine.Option(names = "--lower-limit", description = "lower limit for 'exact' estimation", defaultValue = "0.7")
 	private double lowerLimit;
 
+	@CommandLine.Mixin
+	private final CsvOptions csv = new CsvOptions();
+
 	private final double peakPercentage = 0.09;
 
 	@Override
 	public Integer call() throws Exception {
 
-		Table dtvFromGeoPortal = new DataFrameReader(new ReaderRegistry()).csv(input.getPath("dtv_berlin.csv"));
+		Table dtvFromGeoPortal = Table.read().csv(CsvReadOptions.builderFromFile(input.getPath("dtv_berlin.csv"))
+				.columnTypesPartial(Map.of("from_link", ColumnType.TEXT, "to_link", ColumnType.TEXT))
+				.build());
 
-		Set<Id<Link>> matched = dtvFromGeoPortal.stringColumn("to_link").append(dtvFromGeoPortal.stringColumn("from_link"))
-				.asList().stream().map(Id::createLinkId).collect(Collectors.toSet());
+		Network network = input.getNetwork();
 
-		Network filtered;
-		{
-			Network network = input.getNetwork();
-
-			NetworkFilterManager manager = new NetworkFilterManager(network, new NetworkConfigGroup());
-			manager.addLinkFilter(l -> matched.contains(l.getId()));
-
-			filtered = manager.applyFilters();
-		}
-
-		createMappingOverviewFiles(dtvFromGeoPortal, filtered.getLinks());
+		createMappingOverviewFiles(dtvFromGeoPortal, network.getLinks());
 		dtvFromGeoPortal.intColumn("vol_car").setName("observed_daily_traffic_volume");
 
 		//reading events file & create volumes
 		EventsManager eventsManager = EventsUtils.createEventsManager();
-		VolumesAnalyzer volume = new VolumesAnalyzer(3600, 86400, filtered, true);
+		VolumesAnalyzer volume = new VolumesAnalyzer(3600, 86400, network, true);
 		eventsManager.addHandler(volume);
 		eventsManager.initProcessing();
 		EventsUtils.readEvents(eventsManager, input.getEventsPath());
@@ -89,58 +79,121 @@ public class DTVAnalysis implements MATSimAppCommand {
 
 		Table sim = createSimDtvTable(volume);
 
-		Table toJoin = dtvFromGeoPortal.copy().removeColumns("from_link").dropWhere(dtvFromGeoPortal.stringColumn("to_link").isEmptyString())
-				.joinOn("to_link", "link_id").leftOuter(sim);
+		Table join = dtvFromGeoPortal.copy().joinOn("from_link").leftOuter(setIdColNameTo(sim, "from_link")).joinOn("to_link").leftOuter(setIdColNameTo(sim, "to_link"));
 
-		Table fromJoin = dtvFromGeoPortal.copy().removeColumns("to_link").dropWhere(dtvFromGeoPortal.stringColumn("to_link").isEmptyString())
-				.joinOn("to_link", "link_id").leftOuter(sim);
+		join.removeColumns("vol_freight", "both_directions");
 
-		generateQualityLabel(List.of(toJoin, fromJoin));
+		Table valid = join.copy().where(join.booleanColumn("is_valid").isTrue());
 
-		Table append = toJoin.append(fromJoin).removeColumns("vol_freight", "both_directions");
+		Table longTable = createLongTable(valid);
 
-		Table valid = append.where(append.booleanColumn("is_valid").isTrue());
+		generateQualityLabel(longTable);
 
-		Table dailyShare = valid.copy().summarize("station", AggregateFunctions.count).by("daily_quality");
-		DoubleColumn shareColumn = normalizeColumnNames(dailyShare).intColumn("station").asDoubleColumn().divide(dailyShare.rowCount()).setName("share");
-		dailyShare.addColumns(shareColumn).write().csv(output.getPath("day_quality_per_road_type.csv").toFile());
+		//write data frame for map plot
+		log.info("Write long table to {}", output.getPath("dtv_comparison.csv").toString());
+		writeMapPlotTable(join);
 
-		Table peakShare = valid.copy().summarize("station", AggregateFunctions.count).by("peak_hour_quality");
-		DoubleColumn peakShareColumn = normalizeColumnNames(peakShare).intColumn("station").asDoubleColumn().divide(peakShare.rowCount()).setName("share");
-		peakShare.addColumns(shareColumn).write().csv(output.getPath("day_quality_per_road_type.csv").toFile());
+		log.info("Write daily estimation quality analysis to {}", output.getPath("day_quality_per_road_type.csv").toString());
+		Table dailyShare = longTable.copy().countBy("daily_quality", "road_type");
+		normalizeColumnNames(dailyShare).summarize("n", new NumericAggregateFunction("share") {
+			@Override
+			public Double summarize(NumericColumn<?> column) {
+				return column.sum() / (double) column.size();
+			}
+		}).by("road_type").write().csv(output.getPath("day_quality_per_road_type.csv").toFile());
+
+		log.info("Write peak hour estimation quality analysis to {}", output.getPath("peak_quality_per_road_type.csv").toString());
+		Table peakShare = valid.copy().countBy("peak_hour_quality", "road_type");
+		normalizeColumnNames(peakShare).summarize("n", new NumericAggregateFunction("share") {
+			@Override
+			public Double summarize(NumericColumn<?> column) {
+				return column.sum() / (double) column.size();
+			}
+		}).by("road_type").write().csv(output.getPath("peak_quality_per_road_type.csv").toFile());
 
 		return 0;
 	}
 
-	private void generateQualityLabel(List<Table> tables) {
+	private Table createLongTable(Table table) {
 
-		for (Table table : tables) {
+		Table from = table.copy().selectColumns("from_link", "from_simulated_daily_traffic_volume", "road_type");
+		Table to = table.copy().selectColumns("to_link", "to_simulated_daily_traffic_volume", "road_type");
 
-			for(Column<?> column: table.columns()) {
-				if (column.name().contains("_link"))
-					column.setName("link_id");
-
-				if(column.name().contains("capacity"))
-					column.setName("capacity");
+		for(Table t: List.of(from, to)){
+			for (Column<?> column : t.columns()) {
+				String name = column.name();
+				column.setName(name.replace("from_", "").replace("to_", ""));
 			}
+		}
 
-			DoubleColumn obsPeakHour = table.intColumn("vol_car").copy().multiply(peakPercentage).setName("observed_peak_hour_traffic_volume");
+		return from.append(to);
+	}
 
-			table.addColumns(obsPeakHour, StringColumn.create("peak_hour_quality"), StringColumn.create("daily_quality"));
+	private void writeMapPlotTable(Table table) {
 
+		try (CSVPrinter printer = csv.createPrinter(output.getPath("dtv_valid_and_quality.csv"))) {
 			for (Row row : table) {
-				double observedPeakHourTrafficVolume = row.getDouble("observed_peak_hour_traffic_volume");
-				double observedDailyTrafficVolume = row.getDouble("observed_daily_traffic_volume");
 
-				double simulatedPeakHourTrafficVolume = row.getDouble("simulated_peak_hour_traffic_volume");
-				double simulatedDailyTrafficVolume = row.getDouble("simulated_daily_traffic_volume");
+				String fromLink = row.getText("from_link");
+				if (!fromLink.isBlank()) {
+					Boolean valid = row.getBoolean("is_valid");
+					String quality = row.getString("quality_from");
 
-				String dayQuality = getEstimationQuality(observedDailyTrafficVolume, simulatedDailyTrafficVolume);
-				String peakQuality = getEstimationQuality(observedPeakHourTrafficVolume, simulatedPeakHourTrafficVolume);
+					printer.printRecord(fromLink, valid, quality);
+				}
 
-				row.setString("peak_hour_quality", peakQuality);
-				row.setString("daily_quality", dayQuality);
+				String toLink = row.getText("to_link");
+				if (!fromLink.isBlank()) {
+					Boolean valid = row.getBoolean("is_valid");
+					String quality = row.getString("quality_to");
+
+					printer.printRecord(toLink, valid, quality);
+				}
 			}
+		} catch (IOException e) {
+			log.error("Error printing map plot table", e);
+		}
+	}
+
+	private Table setIdColNameTo(Table sim, String colName) {
+
+		Table copy = sim.copy();
+
+		String direction = colName.split("_")[0];
+
+		for (String name : copy.columnNames()) {
+
+			Column<?> column = copy.column(name);
+			if (name.equals("link_id")) {
+				column.setName(colName);
+			} else {
+				String oldName = column.name();
+				column.setName(direction + "_" + oldName);
+			}
+		}
+
+		return copy;
+	}
+
+	private void generateQualityLabel(Table table) {
+
+		DoubleColumn obsPeakHour = table.intColumn("simulated_daily_traffic_volume").copy().multiply(peakPercentage).setName("observed_peak_hour_traffic_volume");
+		table.addColumns(obsPeakHour, StringColumn.create("peak_hour_quality"), StringColumn.create("daily_quality"));
+
+		for (Row row : table) {
+
+			double observedPeakHourTrafficVolume = row.getDouble("observed_peak_hour_traffic_volume");
+			double observedDailyTrafficVolume = row.getDouble("observed_daily_traffic_volume");
+
+			double simulatedPeakHourTrafficVolume = row.getDouble("simulated_peak_hour_traffic_volume");
+			double simulatedDailyTrafficVolume = row.getDouble("simulated_daily_traffic_volume");
+
+			String dayQuality = getEstimationQuality(observedDailyTrafficVolume, simulatedDailyTrafficVolume);
+			String peakQuality = getEstimationQuality(observedPeakHourTrafficVolume, simulatedPeakHourTrafficVolume);
+
+			row.setString("peak_hour_quality", peakQuality);
+			row.setString("daily_quality", dayQuality);
+
 		}
 	}
 
@@ -160,7 +213,7 @@ public class DTVAnalysis implements MATSimAppCommand {
 
 	private Table createSimDtvTable(VolumesAnalyzer volume) {
 
-		Table simVolumes = Table.create(StringColumn.create("link_id"), DoubleColumn.create("simulated_peak_hour_traffic_volume"),
+		Table simVolumes = Table.create(TextColumn.create("link_id"), DoubleColumn.create("simulated_peak_hour_traffic_volume"),
 				DoubleColumn.create("simulated_daily_traffic_volume"));
 
 		for (Id<Link> id : volume.getLinkIds()) {
@@ -175,7 +228,7 @@ public class DTVAnalysis implements MATSimAppCommand {
 			int dailyTrafficVolume = IntArrayList.of(volumesForLink).intStream().sum();
 
 			Row row = simVolumes.appendRow();
-			row.setString("link_id", id.toString());
+			row.setText("link_id", id.toString());
 			row.setInt("simulated_peak_volume_volume", peakVolume);
 			row.setInt("day_volume_sim", dailyTrafficVolume);
 		}
@@ -223,18 +276,20 @@ public class DTVAnalysis implements MATSimAppCommand {
 			double toCapacity = -1;
 			double fromCapacity = -1;
 
-			String toLink = row.getString("to_link");
+			String toLink = row.getText("to_link");
 
 			if (!toLink.isBlank()) {
 				Link to = links.get(Id.createLinkId(toLink));
+
 				toCapacity = to.getCapacity();
 				type = RoadType.valueOf(NetworkUtils.getHighwayType(to));
 			}
 
-			String fromLink = row.getString("from_link");
+			String fromLink = row.getText("from_link");
 
 			if (!fromLink.isBlank()) {
 				Link from = links.get(Id.createLinkId(fromLink));
+
 				fromCapacity = from.getCapacity();
 
 				RoadType fromType = RoadType.valueOf(NetworkUtils.getHighwayType(from));
