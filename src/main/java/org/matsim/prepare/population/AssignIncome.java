ccf93fca7bac7483009b267e370120880ac43445
rakow <rakow@vsp.tu-berlin.de>
2024-02-07 15:01:25 +0100

    simplify if statements

--- src/main/java/org/matsim/prepare/population/AssignIncome.java
+++ src/main/java/org/matsim/prepare/population/AssignIncome.java
@@ -43,17 +43,16 @@ public class AssignIncome implements PersonAlgorithm {
 		double rndDouble = rnd.nextDouble();
 
 		if (rndDouble <= 0.1) income = 826.;
-		else if (rndDouble > 0.1 && rndDouble <= 0.2) income = 1142.;
-		else if (rndDouble > 0.2 && rndDouble <= 0.3) income = 1399.;
-		else if (rndDouble > 0.3 && rndDouble <= 0.4) income = 1630.;
-		else if (rndDouble > 0.4 && rndDouble <= 0.5) income = 1847.;
-		else if (rndDouble > 0.5 && rndDouble <= 0.6) income = 2070.;
-		else if (rndDouble > 0.6 && rndDouble <= 0.7) income = 2332.;
-		else if (rndDouble > 0.7 && rndDouble <= 0.8) income = 2659.;
-		else if (rndDouble > 0.8 && rndDouble <= 0.9) income = 3156.;
-		else if (rndDouble > 0.9) income = 4329.;
+		else if (rndDouble <= 0.2) income = 1142.;
+		else if (rndDouble <= 0.3) income = 1399.;
+		else if (rndDouble <= 0.4) income = 1630.;
+		else if (rndDouble <= 0.5) income = 1847.;
+		else if (rndDouble <= 0.6) income = 2070.;
+		else if (rndDouble <= 0.7) income = 2332.;
+		else if (rndDouble <= 0.8) income = 2659.;
+		else if (rndDouble <= 0.9) income = 3156.;
 		else {
-			throw new RuntimeException("Aborting..." + rndDouble);
+			income = 4329.;
 		}
 
 		PersonUtils.setIncome(person, income);
