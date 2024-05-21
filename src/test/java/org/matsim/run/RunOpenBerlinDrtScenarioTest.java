6f4aed8bb745ecc2e6671db2ddf8445be6b5f0c6
tschlenther <schlenther@vsp.tu-berlin.de>
2024-05-21 15:51:05 +0200

    try setting up a test for OpenBerlinDrtScenario

---
+++ src/test/java/org/matsim/run/RunOpenBerlinDrtScenarioTest.java
@@ -0,0 +1,29 @@
+package org.matsim.run;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.matsim.application.MATSimApplication;
+import org.matsim.testcases.MatsimTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RunOpenBerlinScenarioTest {
+
+	@RegisterExtension
+	public MatsimTestUtils utils = new MatsimTestUtils();
+
+	@Test
+	public void pct1() {
+
+		int code = MATSimApplication.execute(OpenBerlinScenario.class,
+			"--1pct",
+			"--output", utils.getOutputDirectory(),
+			"--iterations", "2",
+			"--config:simwrapper.defaultDashboards", "disabled"
+		);
+
+		assertThat(code).isEqualTo(0);
+
+	}
+}
