f9bdfbe7027c6b584d2d8b82cc2e5d69d1766248
rakow <rakow@vsp.tu-berlin.de>
2024-09-04 14:58:08 +0200

    bump version to 6.4

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 
 JAR := matsim-berlin-*.jar
-V := v6.3
+V := v6.4
 CRS := EPSG:25832
 
 p := input/$V
--- src/main/java/org/matsim/run/OpenBerlinScenario.java
+++ src/main/java/org/matsim/run/OpenBerlinScenario.java
@@ -30,7 +30,7 @@ import java.util.List;
 @CommandLine.Command(header = ":: Open Berlin Scenario ::", version = OpenBerlinScenario.VERSION, mixinStandardHelpOptions = true)
 public class OpenBerlinScenario extends MATSimApplication {
 
-	public static final String VERSION = "6.3";
+	public static final String VERSION = "6.4";
 	public static final String CRS = "EPSG:25832";
 
 	@CommandLine.Mixin
