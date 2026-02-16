67d19b5f9d61269fa9387aef2e338a60e9b5cfef
Paul Heinrich <55242299+paulheinr@users.noreply.github.com>
2026-02-16 13:57:57 +0100

    update berlin configuration to 10pct

--- src/main/java/org/matsim/run/OpenBerlinScenario.java
+++ src/main/java/org/matsim/run/OpenBerlinScenario.java
@@ -58,7 +58,7 @@ public class OpenBerlinScenario extends MATSimApplication {
 	private String planSelector;
 
 	public OpenBerlinScenario() {
-		super(String.format("input/v%s/berlin-v%s.config.xml", VERSION, VERSION));
+		super(String.format("input/v%s/berlin-v%s-10pct.config.xml", VERSION, VERSION));
 	}
 
 	public static void main(String[] args) {
