b478fedd63ecf9cab19dee00e198a8b8cd3e9f7c
GregorRyb <rybczak@vsp.tu-berlin.de>
2026-03-26 16:31:12 +0100

    simple experiment test cases

---
+++ src/test/java/org/matsim/run/TestParkingChessboard.java
@@ -0,0 +1,185 @@
+package org.matsim.run;
+
+import com.google.inject.Singleton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.contrib.parking.parkingsearch.ParkingSearchStrategy;
+import org.matsim.contrib.parking.parkingsearch.sim.ParkingSearchConfigGroup;
+import org.matsim.contrib.parking.parkingsearch.sim.SetupParking;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.config.groups.RoutingConfigGroup;
+import org.matsim.core.config.groups.ScoringConfigGroup;
+import org.matsim.core.controler.*;
+import org.matsim.core.mobsim.qsim.AbstractQSimModule;
+import org.matsim.core.mobsim.qsim.qnetsimengine.parking.*;
+import org.matsim.core.network.kernel.ConstantKernelDistance;
+import org.matsim.core.network.kernel.DefaultKernelFunction;
+import org.matsim.core.network.kernel.KernelDistance;
+import org.matsim.core.network.kernel.NetworkKernelFunction;
+import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.examples.ExamplesUtils;
+import org.matsim.facilities.*;
+import org.matsim.run.policies.PlanBasedParkingCapacityInitializerBerlin;
+import org.matsim.testcases.MatsimTestUtils;
+
+import static org.matsim.core.mobsim.qsim.qnetsimengine.parking.ParkingUtils.LINK_OFF_STREET_SPOTS;
+import static org.matsim.core.mobsim.qsim.qnetsimengine.parking.ParkingUtils.LINK_ON_STREET_SPOTS;
+
+public class TestParkingChessboard {
+
+	private static final Logger log = LogManager.getLogger(TestParkingChessboard.class);
+
+	@RegisterExtension
+	private MatsimTestUtils utils = new MatsimTestUtils();
+
+	@Test
+	void testParking() {
+		//ParkingProxyConfigGroup parkingProxyConfigGroup = new ParkingProxyConfigGroup();
+
+		Config config = ConfigUtils.loadConfig(IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("chessboard"), "config.xml"));
+
+		config.controller().setLastIteration(0);
+		config.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
+		config.controller().setOutputDirectory("./parkingChessboardOutput");
+		config.routing().setAccessEgressType(RoutingConfigGroup.AccessEgressType.accessEgressModeToLink);
+		config.scoring().setWriteExperiencedPlans(true);
+
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		double sampleSize = scenario.getConfig().qsim().getFlowCapFactor();
+
+		for (Link link : scenario.getNetwork().getLinks().values()) {
+			if (link.getAllowedModes().contains(TransportMode.car)) {
+				//log.info("Parking spots for " + link.getId() + ": on-street=" + parkingSpotsPerLink.get(link.getId()).onstreetSpots + ", off-street=" + parkingSpotsPerLink.get(link.getId()).offstreetSpots);
+				//log.info("Scaled parking spots for " + link.getId() + ": on-street=" + onStreetParkingSpots + ", off-street=" + offStreetParkingSpots);
+				link.getAttributes().putAttribute(LINK_ON_STREET_SPOTS, (int) Math.round(10 * sampleSize));
+				//link.getAttributes().putAttribute(LINK_OFF_STREET_SPOTS, (int) Math.round(5 * sampleSize));
+			}
+		}
+
+		Controler controler = new Controler(scenario);
+
+		controler.addOverridingQSimModule(new AbstractQSimModule() {
+			@Override
+			protected void configureQSim() {
+				addQSimComponentBinding("ParkingOccupancyOberserver").to(ParkingOccupancyObserver.class);
+				addMobsimScopeEventHandlerBinding().to(ParkingOccupancyObserver.class);
+				addVehicleHandlerBinding().to(ParkingVehicleHandler.class);
+				bind(ParkingOccupancyObservingSearchTimeCalculator.class).in(Singleton.class);
+				addParkingSearchTimeCalculatorBinding().to(ParkingOccupancyObservingSearchTimeCalculator.class);
+			}
+		});
+
+		controler.addOverridingModule(new AbstractModule() {
+				@Override
+				public void install() {
+					bind(ParkingOccupancyObserver.class).in(Singleton.class);
+					bind(ParkingCapacityInitializer.class).to(PlanBasedParkingCapacityInitializerBerlin.class);
+					bind(NetworkKernelFunction.class).to(DefaultKernelFunction.class);
+					bind(KernelDistance.class).toInstance(new ConstantKernelDistance(500));
+					// use parameters from Belloche Paper https://pdf.sciencedirectassets.com/308315/1-s2.0-S2352146515X00032/1-s2.0-S2352146515000526/main.pdf?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKn%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJHMEUCICjqKOg8BiZaX434gG0V6S68pKD3x%2BfiiAj529bEHVzEAiEAize6AxQ2S06wRldce%2BX2Pn7tRssYhfhLdYHCcfOm3CMqsgUIEhAFGgwwNTkwMDM1NDY4NjUiDJTkFL2yfN9d7Een5SqPBdoyLYsf38Quz5jeOoOlee6BoBYPWd3pdF5Ft4OFuEtY2vBT8D1mNvwjiIc5o9EMbil%2Bn4J4ubPZLgahREJtFLKvTN%2FSYWaQlaNAr7g%2FFqyOMkNkZ4zRmuTFZHuhUCDkBsFzM7%2BP%2FeB509RzVnNtEFcXnSGmM5CU2QLXxaSpydoaYjNJa7GHot4wTWpAQSPNms98xmQw9SOcZpEEJKs5KbRpzjNzE%2Fw8oOckV4%2FcfXmCNboUbUVp7R%2Bt58iQakgTgumFGS2Mi1sJsVoRu0fc6EnGLsigMrrjftN8xAL7ravb0qhYY%2F1ohHd0LzzFmoJK32pNJonSai6zT97ttRM8PCH7hqoInHlVoRkm7asM8BUdDltEKQwxqG7i37IURCJj5ppO1EDK8YUoiJjgZUDFsl25g0zMGCD4UHpTiFnpl2JkrqHi0QN6du7OwhWUvu7mNUbrAkOleA79iT5FxS6x%2FMIYv2c63rGYxDy5uR5VZcZOS%2FQf5xH9yi5dnjgEMFdFO8%2FfiCyHNUP5a%2B6TQxJvoWcIP9dxZR0CeYQh4hguWeUiiKrFE4Z751wjgqMEEh6%2BktHL6Oh4wcnPm%2BRLjerz1AeQ7lqRyEJ6Jeef3qxbLPZpAum6AYFeKRwQeS6RDDfIgfNgrJVShSskjLUM9rxsgMMVZSreXSRzabXuYIdS%2BnV6sECS%2BahjG2XYX5VXSU%2BDAK8RKqHW323obs4iCBfWx6hMHPuBm57dWfSS829PiOEg7N1Y7u7IhN2nw99t9bwLiSKz6DJO%2FvQg5yeNtO61tYtEj5qxiHu8Sl4Myo3k%2Ba%2FgltmdpjddFAP7Gj%2BWDNc8G1KCN9nFKpNcMQp8%2Fc1evd0irMNgPjIeDi8fZQRpntowzt%2BJvwY6sQF3KQaY7OKk8oCrJwBhqBCXka4LjU%2BmSSEtBfnCSGyP2rGdPMG%2BEa5A4UoXaOve97jMdluCz8qxtStBCcFIGtOlVkduNXiYL%2BEK9Mfsrsh7by6oPxl%2BYjEhxfB%2BcTta4%2BODDQlxAFYuTh9TMyzFkSsrpug2HAxx5wOs02Nyr3i3N%2B1EyurdvJS7wxnn5yUhaglO%2FTrYeIlRXygDrQeOKrcvPBaIHVLm%2Bo7YyQWHH%2BtFFoI%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250325T091640Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAQ3PHCVTYQYKBTS5L%2F20250325%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=c2f30fe9ebfee1301f9171ec0be610f8f6264eef89de1593c609a77eb2eff192&hash=35e4e916f56003c6740345396f4f9903fabcef13107cfa7a35d4b431a1044238&host=68042c943591013ac2b2430a89b270f6af2c76d8dfd086a07176afe7c76c2c61&pii=S2352146515000526&tid=spdf-2ba191d3-1250-4cf4-afd7-42eeada590bd&sid=9d2025d3623a93452e5b8c37c012e71dc9d7gxrqb&type=client&tsoh=d3d3LnNjaWVuY2VkaXJlY3QuY29t&rh=d3d3LnNjaWVuY2VkaXJlY3QuY29t&ua=1e035650550554095a5d0d&rr=925d4f92bb04e531&cc=de
+					//f(occ, K) = alpha * exp(-beta * (occ / K))
+					bind(ParkingSearchTimeFunction.class).toInstance(new BellochePenaltyFunction(2, -6));
+					addControlerListenerBinding().to(ParkingOccupancyObserver.class);
+					addMobsimListenerBinding().to(ParkingOccupancyObserver.class);
+				}
+			});
+		controler.run();
+	}
+
+	@Test
+	void noParking() {
+
+		Config config = ConfigUtils.loadConfig(IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("chessboard"), "config.xml"));
+		config.controller().setOutputDirectory("./chessboardNoParkingOutput");
+		//config.global().setRandomSeed(randomSeed);
+		config.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
+		config.controller().setLastIteration(0);
+		config.routing().setAccessEgressType(RoutingConfigGroup.AccessEgressType.accessEgressModeToLink);
+		config.scoring().setWriteExperiencedPlans(true);
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		Controler controler = new Controler(scenario);
+		controler.run();
+
+	}
+
+
+	@Test
+	void explicitParkingSearch() {
+		Config config = ConfigUtils.loadConfig(IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("chessboard"), "config.xml"));
+		config.controller().setLastIteration(10);
+		config.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
+		config.controller().setOutputDirectory("./withExplicitParkingSearch");
+		config.routing().setAccessEgressType(RoutingConfigGroup.AccessEgressType.accessEgressModeToLink);
+		config.scoring().setWriteExperiencedPlans(true);
+		config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+		config.qsim().setEndTime(50 * 3600);
+
+		ScoringConfigGroup.ActivityParams parkingParams = new ScoringConfigGroup.ActivityParams();
+		parkingParams.setScoringThisActivityAtAll(false);
+		parkingParams.setActivityType("parking");
+		parkingParams.setTypicalDuration(3600.0);
+		config.scoring().addActivityParams(parkingParams);
+
+		ParkingSearchConfigGroup parkingSearchConfigGroup = new ParkingSearchConfigGroup();
+		parkingSearchConfigGroup.setParkingSearchStrategy(ParkingSearchStrategy.Benenson);
+		config.addModule(parkingSearchConfigGroup);
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		Person personToKeep = null;
+		for (Person person: scenario.getPopulation().getPersons().values()) {
+			if(person.getId().equals(Id.createPersonId("1"))) {
+				personToKeep = person;
+			}
+		}
+
+		scenario.getPopulation().getPersons().clear();
+		PopulationUtils.resetRoutes(personToKeep.getSelectedPlan());
+		PopulationUtils.checkRouteModeAndReset(scenario.getPopulation(), scenario.getNetwork());
+		scenario.getPopulation().addPerson(personToKeep);
+
+		Controller controller = ControllerUtils.createController(scenario);
+		SetupParking.installParkingModules(controller);
+		for (Link link: scenario.getNetwork().getLinks().values()) {
+			addParkingFacility(scenario, link.getId().toString() + "_parking", link.getId().toString(), 5);
+		}
+
+		controller.run();
+	}
+
+
+	private void addParkingFacility(
+		Scenario scenario,
+		String facilityId,
+		String linkId,
+		double capacity
+	) {
+		ActivityFacilities facilities = scenario.getActivityFacilities();
+
+		ActivityFacilitiesFactory factory = facilities.getFactory();
+
+		Id<ActivityFacility> facId = Id.create(facilityId, ActivityFacility.class);
+		Id<Link> lId = Id.createLinkId(linkId);
+
+		ActivityFacility facility = factory.createActivityFacility(facId, Id.createLinkId(linkId));
+
+		((ActivityFacilityImpl) facility).setLinkId(lId);
+
+		ActivityOption parking = factory.createActivityOption("parking");
+		parking.setCapacity(capacity);
+
+		facility.addActivityOption(parking);
+		facilities.addActivityFacility(facility);
+	}
+}
