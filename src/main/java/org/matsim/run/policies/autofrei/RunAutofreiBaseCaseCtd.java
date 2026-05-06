67e489ab8cd07982b27275d7053d7c106fd4088e
Paul Heinrich <55242299+paulheinr@users.noreply.github.com>
2026-05-06 21:42:00 +0200

    disable vsp checks

--- src/main/java/org/matsim/run/policies/autofrei/RunAutofreiBaseCaseCtd.java
+++ src/main/java/org/matsim/run/policies/autofrei/RunAutofreiBaseCaseCtd.java
@@ -2,11 +2,22 @@ package org.matsim.run.policies.autofrei;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.application.MATSimApplication;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.run.OpenBerlinScenario;
 
 public class RunAutofreiBaseCaseCtd extends OpenBerlinScenario {
 	public static void main(String[] args) {
 		MATSimApplication.run(RunAutofreiBaseCaseCtd.class, args);
+	}
+
+	@Override
+	protected Config prepareConfig(Config config) {
+		super.prepareConfig(config);
+
+		//disabled because some checks have been introduced after this scenario was created and it now fails.
+		config.vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.warn);
+		return config;
 	}
 
 	@Override
