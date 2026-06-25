8c5bff8f10e776b587a02414db130e6875f97e1b
Daniel Roeder (VSP) <d-roeder@users.noreply.github.com>
2026-06-25 22:44:14 +0200

    Revert "add bicyle config group"
    This reverts commit e8db17b4815219ac57979bad4d1474aa2bea42ac.

--- src/main/java/org/matsim/run/OpenBerlinScenario.java
+++ src/main/java/org/matsim/run/OpenBerlinScenario.java
@@ -161,18 +161,6 @@ public class OpenBerlinScenario extends MATSimApplication {
 		}
 
 		controler.addOverridingModule(new PersonMoneyEventsAnalysisModule());
-		controler.addOverridingModule(new AbstractModule() {
-			@Override
-			public void install() {
-				BicycleConfigGroup bicycleConfig =
-					ConfigUtils.addOrGetModule(
-						getConfig(),
-						BicycleConfigGroup.class
-					);
-
-				bind(BicycleConfigGroup.class).toInstance(bicycleConfig);
-			}
-		});
 	}
 
 	/**
